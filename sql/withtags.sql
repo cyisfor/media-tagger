f416c72cfe4a5dd4b6b25c2f2752715791f5ce08
user <nobody>
2013-12-23 00:43:51 -0800

    This works... INCREDIBLY SLOWLY

--- sql/withtags.sql
+++ sql/withtags.sql
@@ -1,44 +1,13 @@
-wanted 
-wanted(tags) AS (SELECT array(
-        SELECT tags.id 
-            FROM tags INNER JOIN things ON tags.id = things.id WHERE things.neighbors @> ARRAY[unnest.unnest]
-        ) || unnest.unnest AS tags
-    FROM unnest(%(tags)s::bigint[]) AS unnest);
-unwanted
-unwanted(id) AS (
-        SELECT tags.id
-            FROM tags INNER JOIN things ON tags.id = things.id 
-            WHERE things.neighbors && %%(negatags)s::bigint[] 
-            %(notWanted)s
-        UNION
-        SELECT id
-            FROM unnest(%%(negatags)s::bigint[]) AS id);
-notWanted
-AND NOT things.id = ANY(%(tags)s::bigint[]);
-positiveClause
-media INNER JOIN things ON things.id = media.id;
-positiveWhere
-    WHERE (SELECT every(neighbors && wanted.tags) FROM wanted);
-negativeClause
-NOT neighbors && array(select id from unwanted %(anyWanted)s);
-anyWanted
-where id != ANY(array(SELECT unnest(wanted.tags) FROM wanted)); 
-ordering
-    ORDER BY media.added DESC NULLS LAST 
-    OFFSET %(offset)s LIMIT %(limit)s;
 main
 SELECT media.id,media.name,media.type,
     array(SELECT tags.name FROM tags WHERE tags.id = ANY(things.neighbors))
-FROM
-        %(positiveClause)s
-        %(negativeClause)s
-    %(ordering)s;
+FROM media INNER JOIN 
+    listMedia(%(tags)s::bigint[],%(negatags)s::bigint[],%(offset)s,%(limit)s) AS foo 
+    ON foo = media.id 
+    INNER JOIN things ON media.id = things.id
+    ORDER BY media.added DESC NULLS LAST;
 relatedNoTags
-(NOT tags.id = ANY(%%(tags)s::bigint[])) AND;
+(NOT tags.id = ANY(%(tags)s::bigint[])) AND;
 related
 SELECT tags.name FROM tags INNER JOIN things ON tags.id = ANY(things.neighbors) WHERE %(relatedNoTags)s things.id = ANY(
-    SELECT things.id
-FROM
-        %(positiveClause)s
-        %(negativeClause)s
-    %(ordering)s) LIMIT %%(taglimit)s::int;
+    SELECT foo FROM listMedia(%%(tags)s::bigint[],%%(negatags)s::bigint[],%%(offset)s,%%(limit)s) AS foo) LIMIT %%(taglimit)s;
--- withtags.py
+++ withtags.py
@@ -13,59 +13,16 @@ class scalartuple(tuple):
         return scalartuple(super(scalartuple,self).__add__(other))
 
 def searchForTags(tags=None,negatags=None,offset=0,limit=0x30,taglimit=0x10,wantRelated=False):
-    stmt = scalartuple()
-    args = {}
-    if tags.posi or tags.nega:
-        stmt += "WITH "
-        if tags.posi:
-            stmt += stmts['wanted']
-        if tags.nega:
-            if tags.posi:
-                stmt += ','
-                notWanted = stmts['notWanted']
-            else:
-                notWanted = ''
-            stmt += stmts['unwanted'] % {'notWanted': notWanted}
-        if tags.posi:
-            tags.posi = [getTag(tag) if isinstance(tag,str) else tag for tag in tags.posi]
-        if negatags:
-            tags.nega = [getTag(tag) if isinstance(tag,str) else tag for tag in tags.nega]
-    pc = stmts['positiveClause']
-    if tags.posi:
-        pc += ' ' + stmts['positiveWhere']
-    if tags.nega:
-        if tags.posi:
-            negativeClause = 'AND '+stmts['negativeClause']
-            anyWanted = stmts['anyWanted']
-        else:
-            negativeClause = 'WHERE '+stmts['negativeClause']
-            anyWanted = ''
-        negativeClause = negativeClause % {'anyWanted': anyWanted}
-    else:
-        negativeClause = ''
-
     if wantRelated:
-        template = stmts['related']
-        targs = {'relatedNoTags': ((stmts['relatedNoTags'] % {'tags': tags.posi}) if tags.posi else '')}
+        template = stmts['related'] % {'relatedNoTags': (stmts['relatedNoTags'] if tags.posi else '')}
     else:
         template = stmts['main']
-        targs = {}
 
-    targs.update({
-            'positiveClause': pc,
-            'negativeClause': negativeClause,
-            'ordering': stmts['ordering']})
-    stmt += template % targs
+    stmt = template
 
-    stmt = " ".join(stmt)
-    args = {'offset': offset,'limit': limit}
+    args = {'offset': offset,'limit': limit, 'tags': tags.posi, 'negatags': tags.nega}
     if wantRelated:
         args['taglimit'] = taglimit
-    if tags.posi or tags.nega:
-        if tags.posi:
-            args['tags'] = tags.posi
-        if tags.nega:
-            args['negatags'] = tags.nega
     if explain:
         stmt = "EXPLAIN ANALYZE "+stmt
     for row in c.execute(stmt,args):
