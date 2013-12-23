bf472ec7115de33c12eb31f8ca0405919e56b176
user <nobody>
2013-12-23 00:44:48 -0800

    Revert "This works... INCREDIBLY SLOWLY"
    This reverts commit f416c72cfe4a5dd4b6b25c2f2752715791f5ce08.

--- withtags.py
+++ withtags.py
@@ -13,16 +13,59 @@ class scalartuple(tuple):
         return scalartuple(super(scalartuple,self).__add__(other))
 
 def searchForTags(tags=None,negatags=None,offset=0,limit=0x30,taglimit=0x10,wantRelated=False):
+    stmt = scalartuple()
+    args = {}
+    if tags.posi or tags.nega:
+        stmt += "WITH "
+        if tags.posi:
+            stmt += stmts['wanted']
+        if tags.nega:
+            if tags.posi:
+                stmt += ','
+                notWanted = stmts['notWanted']
+            else:
+                notWanted = ''
+            stmt += stmts['unwanted'] % {'notWanted': notWanted}
+        if tags.posi:
+            tags.posi = [getTag(tag) if isinstance(tag,str) else tag for tag in tags.posi]
+        if negatags:
+            tags.nega = [getTag(tag) if isinstance(tag,str) else tag for tag in tags.nega]
+    pc = stmts['positiveClause']
+    if tags.posi:
+        pc += ' ' + stmts['positiveWhere']
+    if tags.nega:
+        if tags.posi:
+            negativeClause = 'AND '+stmts['negativeClause']
+            anyWanted = stmts['anyWanted']
+        else:
+            negativeClause = 'WHERE '+stmts['negativeClause']
+            anyWanted = ''
+        negativeClause = negativeClause % {'anyWanted': anyWanted}
+    else:
+        negativeClause = ''
+
     if wantRelated:
-        template = stmts['related'] % {'relatedNoTags': (stmts['relatedNoTags'] if tags.posi else '')}
+        template = stmts['related']
+        targs = {'relatedNoTags': ((stmts['relatedNoTags'] % {'tags': tags.posi}) if tags.posi else '')}
     else:
         template = stmts['main']
+        targs = {}
 
-    stmt = template
+    targs.update({
+            'positiveClause': pc,
+            'negativeClause': negativeClause,
+            'ordering': stmts['ordering']})
+    stmt += template % targs
 
-    args = {'offset': offset,'limit': limit, 'tags': tags.posi, 'negatags': tags.nega}
+    stmt = " ".join(stmt)
+    args = {'offset': offset,'limit': limit}
     if wantRelated:
         args['taglimit'] = taglimit
+    if tags.posi or tags.nega:
+        if tags.posi:
+            args['tags'] = tags.posi
+        if tags.nega:
+            args['negatags'] = tags.nega
     if explain:
         stmt = "EXPLAIN ANALYZE "+stmt
     for row in c.execute(stmt,args):
