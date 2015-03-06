f81818e997590e01894dd083b455f9252089a3b7
user <nobody>
2015-03-05 21:05:21 -0800

    auto (/extra/user/code/image/tagger/imageInfo/info.c) 274 996 8270

--- imageInfo/info.c
+++ imageInfo/info.c
@@ -1,4 +1,6 @@
+#if memoryok
 #include "memory.h"
+#endif
 
 #include "MagickCore/MagickCore.h"
 
@@ -30,13 +32,59 @@ static void writeerr(const char* message, const char* description) {
   write(1,&nono,2);
 }
 
+static const char* maybeOverrideType(const char* path, bool* cont) {
+  /* we have to cheat here, because ImageMagick "helpfully" converts our svgs
+     into temporary PNG files before assessing the MIME type. 
+  */
+  int inp = open(path,O_RDONLY);
+  if(inp < 0) {
+    writeerr("Could not open",path);
+    *cont = true;
+    return 0x1;
+  }
+
+  char head[0x400] = '';
+  ssize_t amt = read(inp,head,sizeof(head));
+  if(amt < 0) {
+    writeerr("Could not read",path);
+    *cont = true;
+    return 0x1;
+  }
+  close(inp);
+
+  const char* overrideType = NULL;
+        
+  {
+    // SVG
+    const char* espace = head;
+    while(*espace && isspace(*espace)) ++espace;
+    if(*espace) {
+      if(0==strncmp(espace,"<?xml ",sizeof("<?xml "))) {
+        if(strstr(xmlhead+sizeof("<?xml "),"<svg")) {
+          return "image/svg+xml";
+        }
+      }
+    }
+  }
+
+  return NULL;
+}
+        
+
 
 int main(void) {
     char* path = NULL;
     size_t space = 0;
 
+    
+
+#if memoryok
+    memory_pushContext();
+
+    SetMagickMemoryMethods(memory_alloc, memory_realloc, memory_free);
+#endif
+
     MagickCoreGenesis(NULL,MagickFalse);
-    // above is crappy coding so can't use custom memory methods
 
     // This allocates some static memory BECAUSE IMAGEMAGICK SUCKS
     // or maybe libxml sucks
@@ -46,48 +94,32 @@ int main(void) {
     ImageInfo* info = CloneImageInfo(NULL);
     //SetLogEventMask("All");
 
-    SetMagickMemoryMethods(memory_alloc, memory_realloc, memory_free);
-
     for(;;) {
+#if memoryok
         memory_pushContext();
+#endif
+	/* because ImageMagick cannot be memory managed without restarting the
+	   WHOLE process... */
+	if(clock() > 300 * CLOCKS_PER_SEC)
+	  break; // exit if alive for 10min or more
+	alarm(30); // only stay alive and idle for 30s before dying	
+        ssize_t amt = getline(&path,&space,stdin);
+	alarm(0); // try not to die in the middle of getting info		
+        if(amt <= 0) break;
+        if(path[amt-1] == '\n')
+            path[amt-1] = '\0';
+
         ssize_t amt = getline(&path,&space,stdin);
         if(amt <= 0) break;
         if(path[amt-1] == '\n')
             path[amt-1] = '\0';
-        /* we have to cheat here, because ImageMagick "helpfully" converts our svgs
-into temporary PNG files before assessing the MIME type. 
-        */
-        int inp = open(path,O_RDONLY);
-        if(inp < 0) {
-          writeerr("Could not open",path);
-          memory_popContext();
+
+        bool cont = false;
+        const char* overrideType = maybeOverrideType(path,&cont);
+        if(cont)
           continue;
-        }
-
-        char head[0x400] = '';
-        ssize_t amt = read(inp,head,sizeof(head));
-        if(amt < 0) {
-          writeerr("Could not read",path);
-        }
-        close(inp);
-
-        const char* overrideType = NULL;
         
-        {
-          // SVG
-          const char* espace = head;
-          while(*espace && isspace(*espace)) ++espace;
-          if(*espace) {
-            if(0==strncmp(espace,"<?xml ",sizeof("<?xml "))) {
-              if(strstr(xmlhead+sizeof("<?xml "),"<svg")) {
-                overrideType = "image/svg+xml";
-              }
-            }
-          }
-        }
-                           
         strcpy(info->filename,path);
-
         Image* image = ReadImage(info,exception);
 
         if(image == NULL) {
@@ -97,7 +129,6 @@ into temporary PNG files before assessing the MIME type.
             writeString(1,strerror(errno));
             uint16_t nono = htons(errno);
             write(1,&nono,2);
-	    memory_popContext(); // NOW will you free the memory???
             continue;
         }
 
@@ -107,17 +138,18 @@ into temporary PNG files before assessing the MIME type.
         uint16_t height = htons(image->rows);
 
         write(1,"I",1);
-        if(overrideType) {
+        if(overrideType) 
           writeString(1,overrideType);
-        } else {
+        else
           writeString(1,MagickToMime(image->magick));
-        }
         write(1,&frames,1);
         write(1,&width,2);
         write(1,&height,2);
 
         DestroyImage(image);
+#if memoryok
         memory_popContext(); // NOW will you free the memory???
+#endif
     }
     return 0;
 }
--- imageInfo/info.fail.c
+++ imageInfo/info.fail.c
@@ -1,6 +1,4 @@
-#if memoryok
 #include "memory.h"
-#endif
 
 #include "MagickCore/MagickCore.h"
 
@@ -23,19 +21,22 @@ static void writeString(int fd, const char* s) {
     }
 }
 
+static void writeerr(const char* message, const char* description) {
+  write(1,"E",1);
+  writeString(1,message);
+  writeString(1,description);
+  writeString(1,strerror(errno));
+  uint16_t nono = htons(errno);
+  write(1,&nono,2);
+}
+
+
 int main(void) {
     char* path = NULL;
     size_t space = 0;
 
-    
-
-#if memoryok
-    memory_pushContext();
-
-    SetMagickMemoryMethods(memory_alloc, memory_realloc, memory_free);
-#endif
-
     MagickCoreGenesis(NULL,MagickFalse);
+    // above is crappy coding so can't use custom memory methods
 
     // This allocates some static memory BECAUSE IMAGEMAGICK SUCKS
     // or maybe libxml sucks
@@ -45,21 +46,48 @@ int main(void) {
     ImageInfo* info = CloneImageInfo(NULL);
     //SetLogEventMask("All");
 
+    SetMagickMemoryMethods(memory_alloc, memory_realloc, memory_free);
+
     for(;;) {
-#if memoryok
         memory_pushContext();
-#endif
-	/* because ImageMagick cannot be memory managed without restarting the
-	   WHOLE process... */
-	if(clock() > 300 * CLOCKS_PER_SEC)
-	  break; // exit if alive for 10min or more
-	alarm(30); // only stay alive and idle for 30s before dying	
         ssize_t amt = getline(&path,&space,stdin);
-	alarm(0); // try not to die in the middle of getting info		
         if(amt <= 0) break;
         if(path[amt-1] == '\n')
             path[amt-1] = '\0';
+        /* we have to cheat here, because ImageMagick "helpfully" converts our svgs
+into temporary PNG files before assessing the MIME type. 
+        */
+        int inp = open(path,O_RDONLY);
+        if(inp < 0) {
+          writeerr("Could not open",path);
+          memory_popContext();
+          continue;
+        }
+
+        char head[0x400] = '';
+        ssize_t amt = read(inp,head,sizeof(head));
+        if(amt < 0) {
+          writeerr("Could not read",path);
+        }
+        close(inp);
+
+        const char* overrideType = NULL;
+        
+        {
+          // SVG
+          const char* espace = head;
+          while(*espace && isspace(*espace)) ++espace;
+          if(*espace) {
+            if(0==strncmp(espace,"<?xml ",sizeof("<?xml "))) {
+              if(strstr(xmlhead+sizeof("<?xml "),"<svg")) {
+                overrideType = "image/svg+xml";
+              }
+            }
+          }
+        }
+                           
         strcpy(info->filename,path);
+
         Image* image = ReadImage(info,exception);
 
         if(image == NULL) {
@@ -69,6 +97,7 @@ int main(void) {
             writeString(1,strerror(errno));
             uint16_t nono = htons(errno);
             write(1,&nono,2);
+	    memory_popContext(); // NOW will you free the memory???
             continue;
         }
 
@@ -78,15 +107,17 @@ int main(void) {
         uint16_t height = htons(image->rows);
 
         write(1,"I",1);
-        writeString(1,MagickToMime(image->magick));
+        if(overrideType) {
+          writeString(1,overrideType);
+        } else {
+          writeString(1,MagickToMime(image->magick));
+        }
         write(1,&frames,1);
         write(1,&width,2);
         write(1,&height,2);
 
         DestroyImage(image);
-#if memoryok
         memory_popContext(); // NOW will you free the memory???
-#endif
     }
     return 0;
 }
