699c92a82d67059278080392ba55dc435903cd87
user <nobody>
2015-04-21 00:21:26 -0700

    auto (/extra/user/code/image/tagger/message.py) 124 331 3196

--- message.py
+++ message.py
@@ -1,70 +1,62 @@
+class OneToMany:
+    def __init__(self,one):
+        self.one = one
+    def decode(self,message):
+        size = message.pop(0)
+        if size == 0:
+            return 0
+        l = message[:size]
+        del message[:size]
+        return self.one.decode(l)
+    def encode(self,message,v):
+        if not v:
+            # this actually isn't necessary, but is just a shortcut for
+            # what below accomplishes
+            message.push(0)
+            return
+        res = []
+        self.one.encode(res)
+        message.push(len(res))
+        message.extend(res)
+        
 class codecs:
-    class num:
-        @staticmethod
-        def decode(message):
-            size = message.pop(0)
-            if size == 0:
-                return 0
-            l = message[:size]
-            del message[:size]
-            sum = 0
-            for i in l:
-                sum = sum << 8 + i
-            return sum
-        @staticmethod
-        def encode(message,i):
-            if i == 0:
-                # this actually isn't necessary, but is just a shortcut for
-                # what below accomplishes
-                message.push(0)
-                return
-            res = []
-            while i > 0:
-                m = i & 0x100
-                res.append(m)
-                i = i >> 8
-            res.reverse()
-            message.push(len(res))
-            message.extend(res)
-
-    class str:
-        @staticmethod
-        def decode(message):
-            size = message.pop(0)
-            if size == 0:
-                return None
-            arg = message[:size]
-            del message[:size]
-            return bytes(*arg).decode('utf-8')
-
-        @staticmethod
-        def encode(message,s):
-            if s is None:
-                message.append(0)
-                return
-            b = s.encode('utf-8')
-            assert len(b) > 0
-            message.append(len(b))
-            message.extend(b)
-    class onestr:
-        @staticmethod
-        def decode(message):
-            s = bytes(*message).decode('utf-8')
-            message[:] = ()
-            return s
-        @staticmethod
-        def encode(message,s):
-            message[:] = s.encode('utf-8')
-
+    class one:
+        class str:
+            @staticmethod
+            def decode(message):
+                s = bytes(*message).decode('utf-8')
+                del message[:]
+                return s
+            @staticmethod
+            def encode(message,s):
+                message[:] = s.encode('utf-8')
+        class num:
+            @staticmethod
+            def decode(message):
+                sum = 0
+                for i in message:
+                    sum = sum << 8 + i
+                return sum
+            @staticmethod
+            def encode(message,i):
+                while i > 0:
+                    m = i & 0x100
+                    message.append(m)
+                    i = i >> 8
+                message.reverse()
+    str = OneToMany(one.str)
+    num = OneToMany(one.num)
+    
 class Command:
-    codecs = None
-    def __init__(self,f,command,codec=None,codecs=None):
+    codec = None
+    command = None
+    def __init__(self,f,codec=None,codecs=None):
         self.f = f
-        self.command = command
-        if codecs:
-            self.codecs = codecs
-        else if codec:
-            self.codecs = [codec]
+        self.codecs = codecs
+        if codec:
+            self.codec = codec
+        elif 'default' in codecs:
+            self.codec = codecs.pop('default')
     def __get__(self):
         if self.codecs is None:
             return self.f
@@ -74,22 +66,28 @@ class Command:
             args = []
             i = 0
             while(message):
-                codec = self.codecs.get(i)
-                if codec is None:
-                    codec = self.codecs[0]
+                if self.codecs:
+                    codec = self.codecs.get(i)
+                    if codec is None:
+                        codec = self.codec
+                else:
+                    codec = self.codec
                 args.append(codec.decode(message))
                 i = i + 1
             self.f(self,*args)
         else:
             self.f(self,message)
     def __remotelycall__(self,proc,*args):
+        assert self.command is not None
         message = [0,0,self.command]
-        if self.codecs:
-            for i,arg in enumerate(args):
+        for i,arg in enumerate(args):
+            if self.codecs:
                 codec = self.codecs.get(i)
                 if codec is None:
-                    codec = self.codecs[0]
-                codec.encode(message,arg)
+                    codec = self.codec
+            else:
+                codec = self.codec
+            codec.encode(message,arg)
         else:
             message.extend(args[0])
         length = len(message) + 1 # for command
@@ -97,12 +95,16 @@ class Command:
         self.write.send_bytes(bytes(*message))
 
 class CommandEnabler(type):
+    commands = []
     def __new__(cls, name, bases, attrs):
-        cls.commands = []
         for n,attr in attrs.items():
-            if hasattr(attr,'__remotelycall__'
+            if hasattr(attr,'__remotelycall__'):
+                attr.command = len(CommandEnabler.commands)
                 attrs[n] = attr.__remotelycall__
-                cls.commands.append(attr)
+                # all commands must have global IDs
+                # so no subclasses w/ separate lists!
+                cls.commands = CommandEnabler.commands
+                CommandEnabler.commands.append(attr)
         return super(CommandEnabler, cls).__new__(cls, name, bases, newattrs)
 
 class MessageProcess(Process,metaclass=CommandEnabler):
