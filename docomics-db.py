d9dd310aa99598c89dbb380f9921022fe6dd6217
user <nobody>
2015-07-24 19:12:54 -0700

    auto (/extra/user/code/image/tagger/docomics-db.py) 54 112 1517

--- docomics-db.py
+++ docomics-db.py
@@ -12,6 +12,14 @@ def R(a):
     if isinstance(a,socket.socket): return a.fileno()
     return a
 
+def note(*a):
+    import sys
+    for e in a:
+        sys.stdout.write(str(R(e)))
+        sys.stdout.write(' ')
+    sys.stdout.write('\n')
+    sys.stdout.flush()
+
 replying = {}
 replyid = count(0)
 
@@ -44,10 +52,10 @@ class ProxyMethod:
         returnid = None
         if self.returns:
             returnid = next(replyid)
-        print(self.proxy,'calling',self.name)
+        note(self.proxy,'calling',self.name)
         self.proxy.send(json.dumps(
             [self.name,returnid,a,kw]).encode('utf-8'))
-        self.proxy.waitFor(returnid)
+        return self.proxy.waitFor(returnid)
 
 class Proxy:
     def __repr__(self):
@@ -60,7 +68,7 @@ class Proxy:
         self.buffer = bytearray(0x1000)
         self.pos = 0
     def send(self,b):
-        print('send',R(self.socket))
+        note('send',R(self.socket))
         return self.socket.send(b)
     def __getattr__(self,name):
         assert name != 'waitFor'
@@ -71,23 +79,26 @@ class Proxy:
                            method)
     def waitFor(self,returnid=None):
         while True:
-            print('wait for',R(self.socket))
+            note('wait for',returnid,R(self.socket))
             amt = self.socket.recv_into(memoryview(self.buffer)[self.pos:])
-            print(self,'got bit',R(self.socket),
+            if not amt: break
+            note(self,'got bit',amt,R(self.socket),
                   self.inward,
                   bytes(memoryview(self.buffer)[self.pos:amt]))
             self.pos += amt
             try:
                 message = json.loads(bytes(memoryview(self.buffer)[:self.pos]).decode('utf-8'))
+                if not message: break
             except ValueError: continue
             except Exception as e:
-                print(type(e))
+                note(type(e))
                 raise
             else:
                 end = len(self.buffer) - self.pos
                 self.buffer[:] = self.buffer[self.pos:]
                 self.pos = end
                 id,*message = message
+                note('message',id,message)
                 if isinstance(id,str):
                     realmethod = getattr(self.inward,id)
                     returns,a,kw = message
@@ -98,10 +109,14 @@ class Proxy:
                     ret = realmethod(*a,**kw)
                     if returns is not None:
                         self.socket.send(json.dumps([returns,ret]).encode('utf-8'))
-                if returnid is not None and id == returnid:
+                elif returnid is not None and id == returnid:
                     ret = message[0]
-                    for i,v in enumerate(ret):
-                        ret[i] = proxifyCallables(self.socket,v)
+                    print('returning',ret)
+                    try: 
+                        for i,v in enumerate(ret):
+                            ret[i] = proxifyCallables(self.socket,v)
+                    except TypeError:
+                        ret = proxifyCallables(self.socket,ret)
                     return ret
                 else:
                     a,kw = message
@@ -110,18 +125,19 @@ class Proxy:
                     for n,v in kw.items():
                         kw[n] = proxifyCallables(self.socket,v)
                     reply = replying[id]
+                    note('found reply',id,reply)
                     del replying[id]
                     reply(*a,**kw)
 
 class Importer:
     def run(self):
         import favorites.parsers
-        self.gui.waitFor(None)
+        return self.gui.waitFor(None)
     def gotClipboard(self,uri,c,w):
         from favorites.parseBase import parse, ParseError, normalize
         import comic
         url = url.strip()
-        print("Trying {}".format(url))
+        note("Trying {}".format(url))
         sys.stdout.flush()
         try: m = parse(normalize(url))
         except ParseError:
@@ -130,10 +146,10 @@ class Importer:
                        ,lambda *a: self.comicReady(c,w,m))
     def openComic(self) -> int:
         import db
-        return db.execute("SELECT (SELECT MAX(id)+1 FROM comics)")
+        return db.execute("SELECT (SELECT MAX(id)+1 FROM comics)")[0][0]
     def maxWhich(self,c) -> int:
         import db
-        return db.execute("SELECT (SELECT MAX(which) FROM comicPage WHERE comic = $1",(c,))
+        return db.execute("SELECT (SELECT MAX(which) FROM comicPage WHERE comic = $1",(c,))[0][0]
     def comicReady(self,c,w,m):
         import comic
         c,w = self.gui.getStuff()
@@ -165,8 +181,6 @@ class GUI:
         GLib.idle_add(once(self.setup))
         Gtk.main()
     def setup(self,um=None):
-        print('um?',um)
-        print('ocom',self.db.openComic())
         from gi.repository import Gtk
         window = Gtk.Window()
         window.connect('destroy',Gtk.main_quit)
@@ -186,16 +200,17 @@ class GUI:
     def setWhich(self,which):
         self.wentry.set_text('{:x}'.format(which))
     def gotClipboard(self,uri):
-        c = self.centry.get_text()
-        if not c:
-            c = self.db.openComic()
-            self.centry.set_text('{:x}'.format(c))
         w = self.wentry.get_text()
         if w:
             w = int(w,0x10)
         else:
-            w = self.db.maxWhich()+1
-            self.wentry.set_text('{:x}'.format(w))
+            w = self.db.maxWhich()
+            if w is not None:
+                self.wentry.set_text('{:x}'.format(w+1))
+                c = self.centry.get_text()
+                if not c:
+                    c = self.db.openComic()
+                    self.centry.set_text('{:x}'.format(c))
         self.db.gotClipboard(uri,c,w)
     def getInfo(self,next):
         from gi.repository import Gtk
@@ -235,7 +250,7 @@ gui,importer = socket.socketpair()
 g.db = Proxy('Importer',i,g,importer)
 i.gui = Proxy('GUI',g,i,gui)
 
-print('gi',R(gui),R(importer))
+note('gi',R(gui),R(importer))
 
 
 pid = os.fork()
@@ -246,7 +261,7 @@ if pid == 0:
 
 try:
     g.run()
-    os.waitpid(pid)
+    os.waitpid(pid,0)
     pid = None
 finally:
     if pid is not None:
