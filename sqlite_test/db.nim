9b4bdec3c7cfb23c29ffe805881cea1bbc8066a5
user <nobody>
2015-08-23 21:54:38 +0000

    auto (/extra/user/code/image/tagger/sqlite_test/db.nim) 51 139 1248

--- sqlite_test/db.nim
+++ sqlite_test/db.nim
@@ -6,7 +6,7 @@ const onequery = "SELECT medium FROM media_tags WHERE tag = (SELECT id FROM tags
 proc startdb*() =
   open("pics.sqlite",conn)
 
-iterator list*(posi: seq[string],nega: seq[string], limit: int, offset: int): tuple[medium: int,title: string] =
+iterator list*(posi: seq[string],nega: seq[string], limit: int, offset: int): tuple[medium: int,title: string] {.inline.} =
   var query = ""
   if posi.len == 0 and nega.len == 0:
     query = "SELECT id FROM media"
@@ -20,24 +20,21 @@ iterator list*(posi: seq[string],nega: seq[string], limit: int, offset: int): tu
         if query != "":
           query = query & " EXCEPT "
         query = query & onequery
-  query = query & " LIMIT ?"
-  query = query & " OFFSET ?"
+  query = query & " LIMIT " & $limit
+  query = query & " OFFSET " & $offset
   echo("query is ",escape(query))
   query = "SELECT id,name FROM media WHERE id IN (" & query & ")"
-  var st = prepare(conn,query)
-  echo("yay?")
-  var which = 0
-  for tag in posi:
-    st.Bind(which,tag)
-    which += 1
-  for tag in nega:
-    st.Bind(which,tag)
-    which += 1
-  st.Bind(which,limit)
-  which += 1
-  st.Bind(which,offset)
-  for _ in st.foreach():
-    var medium: int = column_int(st,0)
-    var title: string = column(st,1)
-    var res = (medium: medium, title: title)
-    yield res
+  withPrep(st,conn,query):
+    echo("yay?")
+    var which = 0
+    for tag in posi:
+      st.Bind(which,tag)
+      which += 1
+    for tag in nega:
+      st.Bind(which,tag)
+      which += 1
+    for _ in st.foreach():
+      var medium: int = column_int(st,0)
+      var title: string = column(st,1)
+      var res = (medium: medium, title: title)
+      yield res
--- sqlite_test/sqldelite.nim
+++ sqlite_test/sqldelite.nim
@@ -7,7 +7,7 @@ import strutils
 #  create_function(c,name,nArg,SQLITE_UTF8,nil,fnc,nil,nil);
 
 type CheckDB* = PSqlite3
-type CheckStmt* = tuple
+type CheckStmt* = object
   db: CheckDB
   st: PStmt
 
@@ -26,16 +26,6 @@ proc check(db: CheckDB, res: cint) =
 proc check(st: CheckStmt, res: cint) {.inline.} =
   check(st.db,res)
 
-proc `=`(st: var CheckStmt, src: CheckStmt):
-  st.st = src.st
-  src.st = nil # trading ownership
-  
-proc `=destroy`(st: var CheckStmt) =
-  echo("destructodisk")
-  if st.st != nil:
-    check(st,finalize(st.st))
-    st.st = nil
-  
 proc Bind*(st: CheckStmt, idx: int, val: int) =
   st.check(bind_int(st.st,idx.cint,val.cint))
 
@@ -80,27 +70,32 @@ proc open*(location: string, db: var CheckDB) =
   if (res != SQLITE_OK):
     raise DBError(msg: "Could not open")
 
-proc prepare*(db: CheckDB, sql: string): CheckStmt =
+template withPrep*(st, derpdb, sql: expr, actions: stmt) {.immediate.} =
   var st: CheckStmt
-  st.db = db
-  db.check(prepare_v2(db,sql,sql.len.cint,st.st,nil))
-  return st
-
-template withTransaction*(db: expr, actions: stmt) =
-  var begin = db.prepare("BEGIN")
-  var commit = db.prepare("COMMIT")
-  var rollback = db.prepare("ROLLBACK")
-  begin.step()
+  st.db = derpdb
+  echo("b4",sql)
+  derpdb.check(prepare_v2(derpdb,sql,sql.len.cint,st.st,nil))
+  echo("after")
   try:
     actions
-    commit.step()
-  except:
-    rollback.step()
-    raise
+  finally:
+    check(st,finalize(st.st))  
+
+template withTransaction*(db: expr, actions: stmt) =
+  withPrep(begin,db,"BEGIN"):
+    withPrep(commit,db,"COMMIT"):
+      withPrep(rollback,db,"ROLLBACK"):
+        begin.step()
+        try:
+          actions
+          commit.step()
+        except:
+          rollback.step()
+          raise
 
 proc exec*(db: CheckDB, sql: string) =
-  var st = prepare(db,sql)
-  db.check(step(st.st))
+  withPrep(st,db,sql):
+    db.check(step(st.st))
 
 proc getValue*(st: CheckStmt): int =
   assert(1==column_count(st.st))
@@ -113,5 +108,5 @@ proc getValue*(st: CheckStmt): int =
       raise DBError(msg:"Didn't return a single row",res:res)
 
 proc getValue*(db: CheckDB, sql: string): int =
-  var st = db.prepare(sql)
-  return st.getValue()
+  withPrep(st,db,sql):
+    return st.getValue()
