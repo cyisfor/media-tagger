a930ffd6f2f328356a5b28734bef8fc382004a06
user <nobody>
2015-10-29 18:16:29 +0000

    auto (/extra/user/code/image/tagger/concur.py) 51 148 1276

--- concur.py
+++ concur.py
@@ -1,5 +1,7 @@
 import queue,threading
 
+class Terminator: pass
+
 class RemoteCaller:
     def __init__(self,thread,f,wait=False):
         self.f = f
@@ -38,11 +40,12 @@ class Thread(threading.Thread):
     def run(self):
         while True:
             w = self.queue.get()
+            if w is Terminator: break
             if w.returner:
                 ret = w.f(*w.a,**w.kw)
                 w.returner.set(ret)
             else:
-                assert(w.f(*w.a,**w.kw) is None,str(f)+" shouldn't return a value!")
+                assert w.f(*w.a,**w.kw) is None,str(f)+" shouldn't return a value!"
     def schedule(self,f,a,kw,returning):
         if returning:
             returner = Returner()
@@ -50,16 +53,60 @@ class Thread(threading.Thread):
             return returner.get()
         else:
             self.queue.put(Work(f,a,kw,None))
+    def finish(self):
+        self.queue.put(Terminator)
+        self.join()
             
 def threadify(k):
-    thread = Thread()
+    thread = Thread(100)
+    k.finish = thread.finish
     for n in dir(k):
         f = getattr(k,n)
-        if(hasattr(f,'threadexport'):
+        if hasattr(f,'threadexport'):
             setattr(k,n,RemoteCaller(thread,f),hasattr(f,'threadwait'))
 
 def export(wait=True):
-    def deco(f):
+    # wait might just be a function so we can do implied @export()
+    if wait is True or wait is False:
+     def deco(f):
+         f.threadexport = True
+         f.threadwait = wait
+         return f
+     return deco
+    else:
+        f = wait
         f.threadexport = True
-        if wait: f.threadwait = True
+        f.threadwait = True
         return f
+
+def test():
+    import time
+    @threadify
+    class StupidBlocker:
+        @export
+        def foo(self):
+            time.sleep(3)
+            return 'bar'
+        @export(wait=False)
+        def foo2(self):
+            time.sleep(3)
+            print("can't return a bar")
+        def foo3(self):
+            time.sleep(3)
+            return 'bar3'
+
+    start = time.time()
+    def elapsed():
+        e = time.time()-start
+        return int(e*10)/10
+    b = StupidBlocker()
+    print(elapsed(),'foo',b.foo())
+    print(elapsed(),'foo2',b.foo2())
+    print(elapsed(),'foo3',b.foo3())
+    print(elapsed(),'finishing...')
+    b.finish()
+    print(elapsed(),'b done')
+
+if __name__ == '__main__':
+    test()
+    
