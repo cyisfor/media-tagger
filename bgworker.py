11f1443429937292ac5f4fee005f83f353da97ed
user <nobody>
2016-01-14 05:44:58 +0000

    auto (/extra/user/code/image/tagger/bgworker.py) 54 1680

--- bgworker.py
+++ bgworker.py
@@ -1,42 +1,37 @@
 import threading,queue
-from mygi import GLib
+def makeWorker(init,foreground):
+    bgqueue = queue.Queue()
+    def background(f):
+        bgqueue.push(f)
 
-bgqueue = queue.Queue()
-def background(f):
-    bgqueue.push(f)
+    @threading.Thread
+    def bgThread():
+        init()
+        bgqueue.put(True)
+        while True:
+            try:
+                try: g,backToforeground = result
+                except ValueError:
+                    # eh, just a function, will do its own foregrounding
+                    result()
+                    continue
+                for mode in g:
+                    if mode is foreground:
+                        GLib.idle_add(backToforeground)
+                        return
+            except:
+                import traceback
+                traceback.print_exc()
+                print('background thread exception!!')
+    bgqueue.get() # okay stuff is imported and connected to db IN THE BACKGROUND
 
-def foreground(f):
-    GLib.idle_add(f)
-
-@threading.Thread
-def bgThread():
-    import comic,db
-    from favorites.parseBase import parse, ParseError, normalize
-    import favorites.parsers # side effects galore!
-    bgqueue.put(True)
-    while True:
-        try:
-            try: g,backToforeground = result
-            except ValueError:
-                # eh, just a function, will do its own foregrounding
-                result()
-                continue
-            for mode in g:
-                if mode is foreground:
-                    GLib.idle_add(backToforeground)
-                    return
-        except:
-            import traceback
-            traceback.print_exc()
-            print('background thread exception!!')
-bgqueue.get() # okay stuff is imported and connected to db IN THE BACKGROUND
-
-def dually(f):
-    def wrapper(*a,**kw):
-        g = f(*a,**kw)
-        def inGUI():
-            for mode in g:
-                if mode is background:
-                    bgqueue.push(g,inGUI)
-                    return
-        inGUI()
+    def dually(f):
+        def wrapper(*a,**kw):
+            g = f(*a,**kw)
+            def inGUI():
+                for mode in g:
+                    if mode is background:
+                        bgqueue.push(g,inGUI)
+                        return
+            inGUI()
+    return dually,background
