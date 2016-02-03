7cbebea7a36d9548de9bb3b66fa1625d1de8787d
user <nobody>
2016-02-03 22:48:59 +0000

    auto (/extra/user/code/image/tagger/sqlite_test/note.nim) 69 1592

--- sqlite_test/note.nim
+++ sqlite_test/note.nim
@@ -1,8 +1,25 @@
 type Notepad* = ref object of RootObj
-  location = false
-  minlevel: int = 2 # ehhhh
+  location: bool
+  minlevel: int
 
 var Notepad root
+
+root.location = true
+
+proc note(level: string, format: string, args: varargs[string, `$`]) {.noSideEffect.} =
+  var s: string
+  if root.location:
+    var frame = getFrame().prev
+    s = "("
+    if root.byProcedure:
+      s = s & frame.procname
+    else:
+      s = s & frame.filename
+      s = s & ":" & $(frame.line) & ") "
+      s = s & level & ": " & strutils.format(format, args))
+  else:
+    s = level & ": " & strutils.format(format, args)
+    debugEcho(s)
 
 from macros import
   bindSym,
@@ -10,13 +27,21 @@ from macros import
   newStrLitNode,
   newNimNode,
   nnkStmtList
-  
-macro setup(level: expr names: expr, notefunc: stmt): stmt =
+
+macro setup(levelvar: expr,
+            minlevel: expr,
+            names: stmt): stmt =
   result = newNimNode(nnkStmtList)
-  add(result,nnkTypeDef(level,nnkEmpty(),
-                        nnkEnumTy(nnkEmpty(),
-                                  names)))
-  add(result,notefunc)
+  # type Level = enum ...info,...
+  var enunames: nnkEnumTy(nnkEmpty())
+  for name in names:
+    add(enunames,name)
+  add(result,nnkTypeDef(levelvar,
+                        nnkEmpty(),
+                        enunames))
+  # root.minlevel = info
+  add(result,nnkAsgn(nnkIdent(!"root"),minlevel))
+
   for name in names:
     # template info*(...)
     add(result,nnkTemplateDef(nnkPostfix(nnkIdent(!"*"), name), # export
@@ -41,7 +66,7 @@ macro setup(level: expr names: expr, notefunc: stmt): stmt =
                                 nnkIfStmt(
                                   nnkElifBranch(
                                     nnkInfix(
-                                      nnkIdent(">"),
+                                      nnkIdent(">="),
                                       name,
                                       nnkDotExpr(
                                         nnkIdent(!"root")
@@ -49,32 +74,15 @@ macro setup(level: expr names: expr, notefunc: stmt): stmt =
                                     nnkCall(nnkIdent(!"note"),
                                             nnkIdent("format"),
                                             nnkIdent("args")))))))
-                                    
-                                               
-setup(Level,[
-  spam,
-  debug,
-  chatty,
-  info,
-  warn,
-  error,
-  always]):      
-  
-  proc note(format: string, args: varargs[string, `$`]) {.noSideEffect.} =
-    var s: string
-    if root.location:
-      var frame = getFrame().prev
-      s = "("
-      if root.byProcedure:
-        s = s & frame.procname
-      else:
-        s = s & frame.filename
-      s = s & ":" & $(frame.line) & ") "
-      s = s & strutils.format(format, args))
-    else:
-      s = strutils.format(format, args)
-    debugEcho(s)
 
 
+setup(Level,info):
+  spam
+  debug
+  chatty
+  info
+  warn
+  error
+  always
 
-  
+info("test $1","hi")
