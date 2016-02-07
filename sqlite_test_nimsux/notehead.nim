c15e9999e9957a1261b71611695e196055bb6c00
user <nobody>
2016-02-07 07:11:08 +0000

    gurgh

--- sqlite_test_nimsux/notehead.nim
+++ sqlite_test_nimsux/notehead.nim
@@ -33,6 +33,31 @@ proc note(level: string, format: string, args: varargs[string, `$`]) =
     s = level & ": " & strutils.format(format, args)
     debugEcho(s)
 
+macro setup(): stmt {.immediate.} =
+  result = newNimNode(nnkStmtList)
+  # type Level = enum ...info,...
+  # (type (A = b, c, d...))
+  var enums = [Level]
+
+  var i = 1
+  while i < len(enums):    
+    var ident = enums[i]
+    echo(ident)
+    var name = newIdentNode(toLower($ident.ident))
+    var namestr = toStrLit(enums[i])
+    i = i + 1
+    # template info*(...)
+    let derp = quote do:
+      template `name`*(format: expr, args: varargs[expr]) =
+        if root.minlevel > `ident`:
+          note(`namestr`, format, args)
+    copyChildrenTo(derp,result)
+  echo(treeRepr(result))
+setup()
+
+
+info("test $1","hi")
+
 template DERP*(format: expr, args: varargs[expr]) =
   if root.minlevel > HERPDERP:
     note("DERPSTR", format, args)
