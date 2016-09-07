0ca8987f1b13251d046eaa2abd28b1a3fb95ccdf
user <nobody>
2016-09-07 18:49:42 +0000

    auto (parse_ui.py) 422 5378

--- favorites/application.py
+++ favorites/application.py
@@ -1,12 +1,15 @@
 import sys
 
-def application(name):
+def application(name,ui,activate=None):
 	from mygi import Gtk,Gio
 	app = Gtk.Application(name,Gio.ApplicationFlags.FLAGS_NONE)
-	def deco(activate):
+	def startup(app):
+		# change the meaning of import application for the ui module
+		sys.modules[__name__] = app
+		__import__(ui)
+	app.connect('startup',startup)
+	if activate:
 		app.connect('activate',activate)
-
-		app.run(sys.argv)
-	return deco
+	app.run(sys.argv)
 
 sys.modules[__NAME__] = application
--- favorites/parse.py
+++ favorites/parse.py
@@ -2,102 +2,10 @@
 import sys
 import os
 
-import application
-application('media.watcher')
-
 import syspath
 import fixprint
 from dbqueue import enqueue
 
-def doparsethingy2():
-	import fcntl,os,time
-	from itertools import count
-	import gtkclipboardy as clipboardy
-	from mygi import Gtk, GLib, GdkPixbuf, Gdk, GObject
-	GObject.threads_init()
-	from catchup import Catchup, Empty
-	print('loading UI')
-	ui = Gtk.Builder.new_from_file(os.path.join(here,"parseui.xml"))			
-	progress = ui.get_object("progress")
-	progress.set_name("progress")
-	css = Gtk.CssProvider()
-	css.load_from_path(os.path.join(here,"parseui.css"))
-
-	# get the default screen for the default display
-	screen = Gdk.Screen.get_default()
-
-	# new object which will store styling information affecting widget
-	styleContext = Gtk.StyleContext()
-	styleContext.add_provider_for_screen(screen, css, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
-
-	def gui_progress(cur,total):
-		progress.set_fraction(cur/total)
-		progress.show()
-
-	busy = GdkPixbuf.PixbufAnimation.new_from_file(
-		os.path.join(here,"sweetie_thinking.gif"))
-	ready = GdkPixbuf.Pixbuf.new_from_file(
-		os.path.join(here, "squeetie.png"))
-	win = ui.get_object("top")
-
-	processed = 0
-	def set_remaining(remaining):
-		nonlocal processed
-		win.set_tooltip_text("%d→%d"%(remaining,processed))
-		processed += 1
-		
-	def set_busy(is_busy=True):
-		print('set busy',is_busy)
-		if not is_busy:
-			progress.hide()
-			img.set_from_pixbuf(ready)
-			win.set_keep_above(False)
-			win.set_icon(ready)
-			return
-		win.set_keep_above(True)
-		win.set_icon(busy)
-		img.set_from_animation(busy)
-		progress.set_fraction(0)
-
-	import catchup
-	catchup = Catchup(provide_progress=True)
-	# whyyyyy
-	def watch_catchup():
-		while True:
-			type,mess = catchup.get()
-			if type == catchup.PROGRESS:
-				GLib.idle_add(lambda mess=mess: gui_progress(*mess))
-			elif type == catchup.IDLE:
-				GLib.idle_add(lambda idle=mess: set_busy(not idle))
-			elif type == catchup.COMPLETE:
-				GLib.idle_add(lambda remaining=mess: set_remaining(remaining))
-			else:
-				print(type,mess)
-				raise SystemExit("wat")
-	import threading
-	t = threading.Thread(target=watch_catchup)
-	t.setDaemon(True)
-	t.start()
-	img = ui.get_object("image")
-	def gotPiece(piece):
-		print("Trying {}".format(piece.strip().replace('\n',' ')[:90]))
-		sys.stdout.flush()
-		enqueue(piece.strip())
-		catchup.poke()
-		print("poked")
-	print('Ready to parse')
-	def seriouslyQuit(win,e):
-		print("Gettin' outta here!",e.button,dir(e.button))
-		Gtk.main_quit()
-		catchup.terminate()
-		raise SystemExit
-	win.connect("button-release-event",seriouslyQuit)
-	win.connect('destroy',Gtk.main_quit)
-	win.set_title('parse')
-	win.show_all()
-	print('Okay!')
-	clipboardy.run(gotPiece,lambda piece: b'http' == piece[:4])
-	print('done yay',os.getpid())
 
 def doparsethingy():
 	try:
@@ -123,4 +31,5 @@ if __name__ == '__main__':
 			catchup.poke()
 		catchup.finish()
 	else:
-		doparsethingy()
+		import application
+		application('media.watcher','parse.ui')
