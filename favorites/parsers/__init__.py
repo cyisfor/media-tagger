2c922f82af2b50970c5592c38c8a20d4c318fb39
user <nobody>
2016-09-07 19:33:20 +0000

    auto (danbooru.py) 52 1785

--- favorites/parsers/__init__.py
+++ favorites/parsers/__init__.py
@@ -9,96 +9,96 @@ from . import weasyl
 from . import pixiv
 from . import flickr
 from . import zerochan
-from . import parse as parse
+from ..parse import registerFinder as register
 
 import re
 
-parse.registerFinder("www.zerochan.net",
+register("www.zerochan.net",
         {'extract': zerochan.extract})
 
-parse.registerFinder("furry.booru.org",
+register("furry.booru.org",
         {'extract': danbooru.extract,
          'tags': [Tag('booru','furry')]})
 
-parse.registerFinder(re.compile("wildcritters\..*"),
+register(re.compile("wildcritters\..*"),
         {'extract': danbooru.extract,
          'tags': ['wildcritters'],
          'normalize': danbooru.normalize},
         name="wildcritters")
 
-parse.registerFinder(re.compile("e621\..*"),
+register(re.compile("e621\..*"),
         {'extract': danbooru.extract,
          'tags': ['e621'],
          'normalize': danbooru.normalize},
         name="e621")
 
-parse.registerFinder("mspabooru.com",
+register("mspabooru.com",
         {'extract': danbooru.extract,
          'tags': ['mspaint'],
          'normalize': danbooru.normalize})
 
-parse.registerFinder("bronibooru.com",
+register("bronibooru.com",
         {'extract': danbooru.extract,
             'normalize': danbooru.normalize,
             'tags': ['pony','bronibooru','booru']})
 
-parse.registerFinder("www.bronibooru.com",
+register("www.bronibooru.com",
         {'extract': danbooru.extract,
             'normalize': danbooru.normalize,
             'tags': ['pony','bronibooru','booru']})
 
-parse.registerFinder(re.compile("twentypercentcooler\..*"),
+register(re.compile("twentypercentcooler\..*"),
         {'extract': danbooru.extract,
          'tags': ['twentypercentcooler','pony'],
          'normalize': danbooru.normalize},
         name="twentypercentcooler")
 
-parse.registerFinder(re.compile(".*furaffinity.net"),
+register(re.compile(".*furaffinity.net"),
         {'extract': furaffinity.extract,
          'tags': ['furaffinity'],
          'normalize': furaffinity.normalize},
         name="furaffinity")
 
-parse.registerFinder(re.compile(".*inkbunny.net"),
+register(re.compile(".*inkbunny.net"),
         {'extract': inkbunny.extract,
          'tags': ['inkbunny']},
         name="inkbunny")
 
-parse.registerFinder('rule34.xxx',
+register('rule34.xxx',
         {'extract': rule34.extract,
             'tags': ['rule34']})
 
-parse.registerFinder(re.compile('.*sofurry.com'),
+register(re.compile('.*sofurry.com'),
         {'extract': sofurry.extract,
             'tags': ['sofurry']},
         name="sofurry")
 
-parse.registerFinder(re.compile('.*weasyl.com'),
+register(re.compile('.*weasyl.com'),
         {'extract': weasyl.extract,
             'normalize': danbooru.normalize,
             'tags': ['weasyl']},
         name="weasyl")
 
-parse.registerFinder('derpibooru.org',
+register('derpibooru.org',
         {'extract': derpibooru.extract,
             'normalize': derpibooru.normalize,
             'tags': ['derpibooru','pony']})
 
-parse.registerFinder('derpiboo.ru',
+register('derpiboo.ru',
         {'extract': derpibooru.extract,
             'normalize': derpibooru.normalize,
             'tags': ['derpibooru','pony']})
 
 """ sigh...
-parse.registerFinder('www.pixiv.net',
+register('www.pixiv.net',
         {'extract': pixiv.extract,
             'tags':['pixiv','japan']})
 """
 
 tags = ['flickr','photo']
-parse.registerFinder('flickr.com',
+register('flickr.com',
         {'extract': flickr.extract,
             'tags':tags})
-parse.registerFinder('www.flickr.com',
+register('www.flickr.com',
         {'extract': flickr.extract,
             'tags':tags})
