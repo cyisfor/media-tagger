d2ccb55dfa9a4406e53ff7a8f26abd354cfbb9d1
user <nobody>
2016-09-08 19:16:24 +0000

    auto (syspath.py) 50 528

--- syspath.py
+++ syspath.py
@@ -4,15 +4,30 @@ D = os.path.dirname
 here = D(os.path.abspath(__file__))
 
 print('a',__name__)
-__name__ = "foo.syspath";
+__name__ = "foo.bar.syspath";
 print('b',__name__)
 try: from .. import syspath
 except SystemError as e:
+	print(here)
 	parent,subname = os.path.split(here)
+	print(parent,subname)
 	name = os.path.basename(parent)
+	subname = name + '.' + subname
 	sys.path.append(D(parent))
+	print('this package',subname)
+	__import__(subname)
+	thispkg = sys.modules[subname]
+	del sys.modules[subname]
+	sys.modules['foo.bar'] = thispkg
 	__import__(name)
-	__name__ = name + '.' + subname + '.' + syspath
-	print('new name',__name__)
+	print('parent package',name)
+	parentpkg = sys.modules[name]
+	del sys.modules[name]
+	sys.modules['foo'] = parentpkg
+	print('ok',sorted(sys.modules.keys()))
+	print(__name__)
 	from .. import syspath
+	sys.path.pop()
 	
+del sys.modules['foo.bar']
+del sys.modules['foo']
