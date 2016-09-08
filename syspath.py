331435e1aa172ec0e52ecc0befbc7d19b4cde16a
user <nobody>
2016-09-08 19:37:22 +0000

    auto (syspath.py) 51 544

--- syspath.py
+++ syspath.py
@@ -4,41 +4,41 @@ D = os.path.dirname
 here = D(os.path.abspath(__file__))
 
 def import_parent():
+	global __name__
 	print('a',__name__)
 	__name__ = "foo.bar.syspath";
 	print('b',__name__)
 	parent,subname = os.path.split(here)
 	name = os.path.basename(parent)
+	subname = name + '.' + subname
+	sys.path.append(D(parent))
 
 	__import__(name)
 	parentpkg = sys.modules[name]
 	print('parent package',name,parentpkg)
-	del sys.modules[name]
+
+	sys.path[-1] = here
+	sys.modules['foo'] = parentpkg
+	__import__(subname)
 	if not hasattr(parentpkg,'__file__'):
 		return
-	sys.modules['foo'] = parentpkg
-
-	subname = name + '.' + subname
-	sys.path.append(D(parent))
-	print('uhh',sys.path)
-	__import__(subname)
 	thispkg = sys.modules[subname]
 	print('this package',subname,thispkg)
 	del sys.modules[subname]
 	sys.modules['foo.bar'] = thispkg
-	print('ummm',__import__('foo.__init__'))
 	print('ok',sorted(sys.modules.keys()))
 	print(__name__)
 	from .. import syspath
-	sys.path.pop()
 	
 try: from .. import syspath
 except (SystemError,ValueError) as e:
 	oldname = __name__
 	import_parent()
+	try:
+		del sys.modules['foo']
+		del sys.modules['foo.bar']
+	except KeyError: pass
 	__name__ = oldname
-	
-del sys.modules['foo.bar']
-del sys.modules['foo']
 
 
+print('whee',sys.path)
