6d154521b8d1e9c70472349e4cfe62beb2941e4e
user <nobody>
2016-09-08 19:30:12 +0000

    auto (syspath.py) 61 662

--- syspath.py
+++ syspath.py
@@ -3,31 +3,42 @@ D = os.path.dirname
 # sigh...
 here = D(os.path.abspath(__file__))
 
-print('a',__name__)
-__name__ = "foo.bar.syspath";
-print('b',__name__)
-try: from .. import syspath
-except SystemError as e:
-	print(here)
+def import_parent():
+	print('a',__name__)
+	__name__ = "foo.bar.syspath";
+	print('b',__name__)
 	parent,subname = os.path.split(here)
-	print(parent,subname)
 	name = os.path.basename(parent)
+
+	__import__(name)
+	parentpkg = sys.modules[name]
+	print('parent package',name,parentpkg)
+	del sys.modules[name]
+	if not hasattr(parentpkg,'__file__'):
+		return
+	sys.modules['foo'] = parentpkg
+
 	subname = name + '.' + subname
 	sys.path.append(D(parent))
-	print('this package',subname)
+	print('uhh',sys.path)
 	__import__(subname)
 	thispkg = sys.modules[subname]
+	print('this package',subname,thispkg)
 	del sys.modules[subname]
 	sys.modules['foo.bar'] = thispkg
-	__import__(name)
-	print('parent package',name)
-	parentpkg = sys.modules[name]
-	del sys.modules[name]
-	sys.modules['foo'] = parentpkg
+	print('ummm',__import__('foo.__init__'))
 	print('ok',sorted(sys.modules.keys()))
 	print(__name__)
 	from .. import syspath
 	sys.path.pop()
 	
+try: from .. import syspath
+except (SystemError,ValueError) as e:
+	oldname = __name__
+	import_parent()
+	__name__ = oldname
+	
 del sys.modules['foo.bar']
 del sys.modules['foo']
+
+
