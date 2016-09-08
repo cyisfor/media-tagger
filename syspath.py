adc7aba5695282ead8d2e8ffebef182393afabb4
user <nobody>
2016-09-08 20:14:53 +0000

    auto (syspath.py) 56 601

--- syspath.py
+++ syspath.py
@@ -11,39 +11,40 @@ class Module:
 		self.name = name
 		self.children = set(children)
 
-top = Module(sys.modules[__name__],__name__)
-del sys.modules[__name__]
+mod = sys.modules[__name__]
+#del sys.modules[__name__]
+__name__ = 'syspath'
+top = Module(mod,__name__)
 
 sys.path.append(here)
 
-def import_parent():
-	global __name__
-	print('a',__name__)
-	__name__ = "foo.bar.syspath";
-	print('b',__name__)
-	parent = here
+def import_parent(here):
+	global top
+	upper = here
 	while True:
-		parent,subname = os.path.split(parent)
+		parent,name = os.path.split(here)
 		sys.path[-1] = parent
 		pkg = __import__(name)
 		print('parent package',name,pkg)
 		if not hasattr(pkg,'__file__'):
-			return
+			return upper
 		top = Module(pkg,name,top)
+		upper = here
+		here = parent
 
 def commit_packages(top,prefix=None):
+	print(('commit',prefix,top.name))
 	if prefix is None:
 		name = top.name
 	else:
 		name = prefix + '.' + top.name
-	sys.modules[top.name] = top.pkg
+	sys.modules[name] = top.pkg
 	for child in top.children:
 		commit_packages(child,name)
 
-try: from .. import syspath
-except (SystemError,ValueError) as e:
-	import_parent()
-	commit_packages(top)
+sys.path[-1] = import_parent(here)
+for child in top.children:
+	commit_packages(child)
 	
 print('whee',sys.path)
 print('ummm',sorted(sys.modules.keys()))
