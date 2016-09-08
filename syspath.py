208348b49570fee3f38f9e017577298e5c084bc0
user <nobody>
2016-09-08 20:01:14 +0000

    auto (syspath.py) 129 1304

--- syspath.py
+++ syspath.py
@@ -3,42 +3,47 @@ D = os.path.dirname
 # sigh...
 here = D(os.path.abspath(__file__))
 
+class Module:
+	pkg = None
+	name = None
+	def __init__(self, pkg, name, *children):
+		self.pkg = pkg
+		self.name = name
+		self.children = set(children)
+
+top = Module(sys.modules[__name__],__name__)
+del sys.modules[__name__]
+
+sys.path.append(here)
+
 def import_parent():
 	global __name__
 	print('a',__name__)
 	__name__ = "foo.bar.syspath";
 	print('b',__name__)
-	parent,subname = os.path.split(here)
-	name = os.path.basename(parent)
-	subname = name + '.' + subname
-	sys.path.append(D(parent))
+	parent = here
+	while True:
+		parent,subname = os.path.split(parent)
+		sys.path[-1] = parent
+		pkg = __import__(name)
+		print('parent package',name,pkg)
+		if not hasattr(pkg,'__file__'):
+			return
+		top = Module(pkg,name,top)
 
-	__import__(name)
-	parentpkg = sys.modules[name]
-	print('parent package',name,parentpkg)
+def commit_packages(top,prefix=None):
+	if prefix is None:
+		name = top.name
+	else:
+		name = prefix + '.' + top.name
+	sys.modules[top.name] = top.pkg
+	for child in top.children:
+		commit_packages(child,name)
 
-	sys.path[-1] = here
-	sys.modules['foo'] = parentpkg
-	__import__(subname)
-	if not hasattr(parentpkg,'__file__'):
-		return
-	thispkg = sys.modules[subname]
-	print('this package',subname,thispkg)
-	del sys.modules[subname]
-	sys.modules['foo.bar'] = thispkg
-	print('ok',sorted(sys.modules.keys()))
-	print(__name__)
-	from .. import syspath
-	
 try: from .. import syspath
 except (SystemError,ValueError) as e:
-	oldname = __name__
 	import_parent()
-	try:
-		del sys.modules['foo']
-		del sys.modules['foo.bar']
-	except KeyError: pass
-	__name__ = oldname
-
-
+	commit_packages(top)
+	
 print('whee',sys.path)
+print('ummm',sorted(sys.modules.keys()))
