3411ec59b11205b5450ec0770ef5ea69305663fc
user <nobody>
2016-09-12 19:24:27 +0000

    auto (setup.py) 344 3243

--- mycookiejar/setup.py
+++ mycookiejar/setup.py
@@ -1,11 +1,3 @@
-import os,sys,time
-oj = os.path.join
-
-try:
-	import http.cookiejar as cookiejar
-except ImportError:
-	import cookielib as cookiejar
-
 from orm import create_table,column,references,make_selins
 
 class Tables:
@@ -26,89 +18,22 @@ class Tables:
 		column("port_specified","BOOLEAN"),
 		column("secure","BOOLEAN"))
 
-db = None
-selins = None
+def setup(place,name="cookies.sqlite",policy=None):
+	from mycookiejar import jar
+	import sqlite3
+	jar.db = sqlite3.connect(oj(place,name))
+	jar.db.execute(Tables.domains)
+	jar.db.execute(Tables.urls)
+	jar.db.execute(Tables.cookies)
+	jar.selins = make_selins(jar.db)
 
-def getCookie(url,name,value,etc):
-	@cookieGetter
-	def _():
-		return {'value': value, 'etc': etc}
+	jar.findDomain = selins("domains","domain")()
+	jar.findURL = selins("urls","domain","path")()
+	jar.cookieGetter = selins("cookies","url","name") # don't commit insert
 
-def splitdict(d):
-	k = d.items()
-	# preserve order of keys and values
-	v = tuple(e[1] for e in k)
-	k = tuple(e[0] for e in k)
-	return k,v
+	sys.modules[jar.__name__] = jar.Jar(policy)
+	# let's not do this a second time, thx
+	sys.modules[__name__] = lambda *a: raise RuntimeError("don't setup twice!")
 
-def update(domain,path,name,value,creationTime,**attrs):
-	baseDomain = ".".join(name.rsplit(".",2)[-2:])
-	attrs['value'] = value
-	attrs = splitdict(attrs)
-	with db:
-		url,created = findURL(findDomain(domain)[0],path)
-		def doinsert():
-			db.execute(
-				"INSERT INTO cookies (lastAccessed,createdTime,name,url"
-				+ ",".join(attrs[0]) + ")"
-				+ "VALUES (?1,?1,?2,?3"
-				+ ",".join("?"+str(i+4) for i in range(len(attrs[0])))
-				+ ")",
-				(time.time(),name,url) + attrs[1])
-		if created:
-			doinsert()
-			return
-
-		r = db.execute("SELECT id,creationTime FROM cookies WHERE url = ? AND name = ?", (url, name))
-		if r:
-			ident,oldcreation = r[0]
-			if oldcreation < creationTime:
-				return
-		r = db.execute(
-			"UPDATE cookies SET "
-			+ ",".join(n+" = ?"+str(i+3) for i,n in enumerate(attrs[0]))
-			+ """, lastAccessed = ?1
-			WHERE id = ?2""",
-			(time.time(),ident) + attrs[1])
-		if r.rowcount == 0:
-			# it got deleted?
-			doinsert()
-
-def cookies_for_request(self,urlid,**values):
-	extra_names = tuple(values.keys())
-	extra_values = tuple(values[n] for n in extra_names)
-	return db.execute(
-		"SELECT id FROM cookies WHERE path = ?1"
-		+"".join(" AND " + name + " = ?"+str(i+2) for i,name in enumerate(extra_names)),
-		(urlid)+extra_values)
-
-class Jar(cookiejar.CookieJar):
-	def __init__(self, policy=None):
-		super().__init__(policy)
-		del self._cookies # just to keep us honest
-
-	def _cookies_for_domain(self, baseDomain, request):
-		for urlid,path in db.execute(
-				"SELECT id,path FROM urls WHERE baseDomain = ?1",
-				baseDomain):
-			if not self._policy.path_return_ok(path, request): continue
-			for cookie in db.execute("SELECT id FROM cookies WHERE path = ?",
-															 (urlid,)):
-				if not self._policy.return_ok(cookie, request): continue
-				yield Cookie(cookie)
-	def __cookies_for_request(self, request):
-		for baseDomain,domain in db.execute("SELECT id,domain FROM domains"):
-			if not self._policy.domain_return_ok(domain, request): continue
-			yield from self._cookies_for_domain(baseDomain,request)
-	def _cookies_for_request(self, request):
-		# this MUST return a forward range
-		return tuple(self.__cookies_for_request(request))
-	def set_cookie(self,cookie,creationTime):
-		herderp = dict()
-		for n in dir(cookie):
-			if n.startswith('_'): continue
-			v = getattr(cookie,n)
-			if callable(v): continue
-			herderp[n] = v
-		update(creationTime=creationTime,
-					 **herderp)
+import sys
+sys.modules[__name__] = setup
