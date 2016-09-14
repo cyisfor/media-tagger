25d18742f943d5a4c626bb473f9a970f2764452f
user <nobody>
2016-09-14 21:14:16 +0000

    auto (tagcache.sql) 96 681

--- sql/tagcache.sql
+++ sql/tagcache.sql
@@ -1,3 +1,4 @@
+CREATE SCHEMA IF NOT EXISTS tagcache;
 
 CREATE OR REPLACE FUNCTION tagcache.query(_tags bigint[])
 RETURNS text AS
@@ -18,5 +19,33 @@ BEGIN
 			-- do nothing
 	END;
 	RETURN _name;
+END;
+$$ language 'plpgsql';
+
+CREATE TABLE tagcache.queries (
+	id SERIAL PRIMARY KEY,
+	tags bigint[] UNIQUE,
+	created timestamptz UNIQUE DEFAULT clock_timestamp());
+	
+CREATE OR REPLACE FUNCTION tagcache.expire(_changed_tags bigint[])
+RETURNS int AS $$
+DECLARE
+_tags bigint[];
+_id int;
+_count int default 0;
+BEGIN
+	for _id,_tags in SELECT id,tags FROM tagcache.queries
+			WHERE tags && _changed_tags
+			ORDER BY created DESC LIMIT 1000 LOOP
+	BEGIN
+		DELETE FROM tagcache.queries WHERE id = _id;
+		EXECUTE 'DROP TABLE tagcache.q' | array_to_string(_tags,'_');
+		_count := _count + 1;
+	EXCEPTION
+		WHEN undefined_table THEN
+				 -- do nothing
+	END;
+	END LOOP;
+	RETURN _count;
 END;
 $$ language 'plpgsql';
