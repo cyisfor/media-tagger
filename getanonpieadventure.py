bbd5fd7c29dc258eb6ede7c94ca12cf4150b50ee
user <nobody>
2016-09-24 18:55:15 +0000

    auto (getanonpieadventure.py) 70 640

--- getanonpieadventure.py
+++ getanonpieadventure.py
@@ -1,25 +1,36 @@
+import syspath
 from comic import findComicByTitle, findMedium
+from favorites.parse import parse
 import db
+
+from setupurllib import myretrieve
+
+import re,tempfile,json,io
 
 comic = findComicByTitle("Anon's Pie Adventure")
 
-which,medium = db.execute("SELECT which,medium FROM comic WHERE id = $1 ORDER BY medium DESC LIMIT 1",
+which,medium = db.execute("SELECT which,medium FROM comicPage WHERE comic = $1 ORDER BY medium DESC LIMIT 1",
 													(comic,))[0]
 
-def source():
-	db.execute("SELECT uri FROM urisources WHERE id IN (select unnest(sources) FROM media WHERE id = $1) AND uri SIMILAR TO $2",
-						 (medium,'^https://derpibooru.org/[0-9]+$'))
-	assert(len(source) == 1)
-	return source[0]
-source = source()
+# sigh
+# why can't the explicit ones also link to the next ones?
+# why can't there be an explicit version of the comic?
+overrides = {
+	1107315: 1107280,
+}
+
+print("current",which,hex(medium))
+source = db.execute("SELECT uri FROM urisources WHERE id IN (select unnest(sources) FROM media WHERE id = $1) AND uri SIMILAR TO $2",
+										(medium,'https://derpibooru.org/[0-9]+'))
+assert(len(source) == 1)
+source = source[0][0]
 
 dpat = re.compile("Next: >>([0-9]+)")
 
 def find_next(source):
-	with tempfile.TemporaryFile() as temp:
-		myretrieve(source,temp)
-		temp.seek(0,0)
-		doc = json.load(temp)
+	temp = io.BytesIO()
+	myretrieve(source+'.json',temp)
+	doc = json.loads(temp.getvalue().decode('utf-8'))
 	description = doc['description']
 	m = dpat.match(description)
 	assert m
