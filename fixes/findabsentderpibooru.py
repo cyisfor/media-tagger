4423e0ce2436767dbcb3763b06e94ce21cc40f65
user <nobody>
2016-10-16 23:34:11 +0000

    auto (findabsentderpibooru.py) 53 505

--- fixes/findabsentderpibooru.py
+++ fixes/findabsentderpibooru.py
@@ -19,12 +19,15 @@ def readtobuf(buf):
 		source = next(gen)
 	except StopIteration: return None
 	assert len(buf) > len(source),len(buf)
-	b = source.encode('utf-8')
-	buf[:] = b
+	b = (source + "\n").encode('utf-8')
+	buf[:len(b)] = b
 	return len(b)
 
 with db.transaction():
 	db.execute("CREATE TEMPORARY TABLE absentderpi (uri TEXT)")
+	db.execute("SELECT 23")
 	db.copy("COPY absentderpi (uri) FROM STDIN WITH encoding 'utf8'",readtobuf)
+with db.transaction():
+	print(db.execute("SELECT uri from absentderpi limit 1"))
+	#db.execute("INSERT INTO parseQueue (host,uri) SELECT $1,uri FROM absentderpi WHERE NOT EXISTS(select 1 from parseQueue where parseQueue.uri = absentderpi.uri)", (host,))
 	print("go")
-	db.execute("INSERT INTO parseQueue SELECT $1,uri FROM absentderpi WHERE NOT EXISTS(select 1 from parseQueue where parseQueue.uri = absentderpi.uri)", (host,))
