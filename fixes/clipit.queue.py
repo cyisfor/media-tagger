5c9538806be1e54101c98785f4be071f39a866c3
user <nobody>
2016-10-18 00:01:46 +0000

    auto (clipit.queue.py) 51 568

--- fixes/clipit.queue.py
+++ fixes/clipit.queue.py
@@ -1,12 +1,25 @@
+import os,struct,subprocess
+
+pid = subprocess.Popen(["python3",os.path.expanduser("~/code/image/tagger/favorites/run.dbqueue.py")],
+        stdin=subprocess.PIPE)
+
+remote = pid.stdin
+
 with open(os.path.expanduser("~/.local/share/clipit/history"),"rb") as inp:
-	size = inp.read(1)
+	size = struct.unpack("i",inp.read(4))[0]
 	print(size)
 	assert(size == -1)
 	inp.read(64)
 	while True:
-		size = inp.read(4)
+		size = inp.read(8)
 		if not size: break
-		size = struct.unpack("L",size)
-		data_type = struct.unpack("L",inp.read(4))
+		size,data_type = struct.unpack("ii",size)
 		data = inp.read(size)
-		print(size,data_type,data)
+		if data_type != 1: continue
+		try: data = data.decode('utf-8')
+		except UnicodeDecodeError: continue
+		if 'http://' in data or 'https://' in data:
+			print("got",data)
+			remote.write(data+b'\n')
+
+
