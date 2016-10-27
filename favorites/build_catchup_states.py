390948f405394f80bdf2e63c1bde0f94e072e4fd
user <nobody>
2016-10-27 01:26:05 +0000

    auto (build_catchup_states.py) 70 550

--- favorites/build_catchup_states.py
+++ favorites/build_catchup_states.py
@@ -1,24 +1,37 @@
 to_client = ["PROGRESS", "IDLE", "COMPLETE", "DONE"]
-to_server = ["POKE","ENABLE_PROGRESS","EHUNNO", "DONE"]
+to_server = ["POKE","ENABLE_PROGRESS"]
+
+def dictify(l):
+	num = 0
+	d = {}
+	for name in l:
+		d[name] = num
+		num += 1
+	return d
+to_client = dictify(to_client)
+to_server = dictify(to_server)
+
+to_server["DONE"] = to_client["DONE"]
 
 import sys,os
-here = sys.modules[__name__]
+here = sys.modules[__name__].__file__
 heremod = os.stat(here).st_mtime
 
 import filedb
 other = os.path.join(filedb.temp,"catchup_states.py")
-othermod = os.stat(other).st_mtime
-if othermod < heremod:
+try: othermod = os.stat(other).st_mtime
+except: othermod = None
+if othermod is None or othermod < heremod:
 	# need update
 	with open(other,"wt") as out:
-		messages = dict()
 		for type,names in (("client",to_client),("server",to_server)):
-			num = 0
-			for name in names:
+			for name,num in names.items():
 				out.write(name+" = "+str(num)+"\n")
-				num += 1
 			out.write("lookup_"+type+" = {\n")
-			for name in names:
+			num = 0
+			for num,name in names.items():
 				out.write("\t"+repr(num)+": " + repr(name) + ",\n")
-			out.write("}")
+				num += 1
+			out.write("\t"+repr(messages["DONE"])+": \"DONE\"\n")
+			out.write("}\n\n")
 
