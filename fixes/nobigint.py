d4a65a2ed74ea062cf6fec612686178c0fdb1ab9
user <nobody>
2017-02-07 19:00:11 +0000

    prepping to remove bigint

--- fixes/nobigint.py
+++ fixes/nobigint.py
@@ -1,17 +1,15 @@
 import os
 
-def fix(script,out):
-	if not 'bigint' in script: return False
-	out.write(script.replace("bigint","INTEGER"))
-	return True
-	
+def fix(script,path):
+	if not 'bigint' in script: return
+	with open("tmp","wt") as out:
+		out.write(script.replace("bigint","INTEGER"))
+	os.rename("tmp",path)
 
 for top,ds,ns in os.walk("."):
 	for n in ns:
+		if n == "nobigint.py": continue # yeah...
 		if n.endswith(".py"):
 			with open(path) as inp:
 				script = inp.read()
-			with open("tmp","wt") as out:
-				if not fix(script,out):
-					continue
-			os.rename("tmp",path)
+			fix(script,path)
