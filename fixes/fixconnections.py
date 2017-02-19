55384882e4b688713c7ff65d5d4b9eac8a7bceef
user <nobody>
2017-02-19 09:11:19 +0000

    auto (fixconnections.py) 61 592

--- fixes/fixconnections.py
+++ fixes/fixconnections.py
@@ -14,8 +14,7 @@ count = 0
 
 def doit():
 	global count
-	c.execute("CREATE TABLE IF NOT EXISTS todisconnect (a int, b int)");
-	c.execute("CREATE TABLE IF NOT EXISTS toconnect (a int, b int)");
+	c.execute("CREATE TABLE IF NOT EXISTS toconnect (a int, b int, dis boolean DEFAULT FALSE NOT NULL)");
 	
 	for row in c.execute("select id,name from tags where name LIKE '%:%' ORDER BY id DESC OFFSET $1",(skip,)):
 		wholetag,wholename = row
@@ -31,23 +30,23 @@ def doit():
 			c.execute("UPDATE tags SET complexity = 1 WHERE id = $1",(wholetag,))
 			c.execute("UPDATE tags SET complexity = 1 WHERE id = $1",(tag,))
 		if True:
-			c.execute("INSERT INTO todisconnect (a,b) VALUES ($2,$1)",(category,tag))
-			c.execute("INSERT INTO todisconnect (a,b) VALUES ($1,$2)",(category,tag))
-			c.execute("INSERT INTO todisconnect (a,b) VALUES ($1,$2)",(wholetag,category))
+			c.execute("INSERT INTO toconnect (a,b,dis) VALUES ($1,$2,TRUE)",(category,tag))
+			c.execute("INSERT INTO toconnect (a,b,dis) VALUES ($1,$2,TRUE)",(tag,category))
+			c.execute("INSERT INTO toconnect (a,b,dis) VALUES ($1,$2,TRUE)",(wholetag,category))
 			c.execute("INSERT INTO toconnect (a,b) VALUES ($1,$2)",(tag,wholetag))
 			c.execute("INSERT INTO toconnect (a,b) VALUES ($1,$2)",(category,wholetag))
 		if count % 100 == 0:
 			if count > 1000:
-				c.execute("CREATE INDEX IF NOT EXISTS todisconnect_a  ON todisconnect(a)")
+				c.execute("CREATE INDEX IF NOT EXISTS toconnect_a_dis  ON toconnect(a,dis)")
 				c.execute("CREATE INDEX IF NOT EXISTS toconnect_a  ON toconnect(a)")
 			print('boop')
-			for a, in c.execute("SELECT DISTINCT a FROM todisconnect"):
+			for a, in c.execute("SELECT DISTINCT a FROM toconnect"):
 				print("reconnecting",a)
 				c.execute("""
-UPDATE things SET neighbors = array(SELECT unnest(neighbors) UNION SELECT b FROM toconnect WHERE a = $1 EXCEPT SELECT b FROM todisconnect WHERE a = $1)
-WHERE neighbors && array(SELECT b FROM toconnect WHERE a = $1 EXCEPT SELECT b FROM todisconnect WHERE a = $1)
+UPDATE things SET neighbors = array(SELECT unnest(neighbors) UNION SELECT b FROM toconnect WHERE a = $1 AND NOT dis EXCEPT SELECT b FROM toconnect WHERE a = $1 AND dis)
+WHERE neighbors && array(SELECT b FROM toconnect WHERE a = $1 AND NOT dis EXCEPT SELECT b FROM toconnect WHERE a = $1 AND dis)
 				""",(a,))
-				
+			c.execute("
 			db.retransaction()
 			with open("derpcounter","w") as out:
 				out.write(str(skip+count))
