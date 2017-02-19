a3ed8738aa7676f7603d123e2202460c39933ae4
user <nobody>
2017-02-19 09:25:37 +0000

    auto (fixconnections.py) 83 626

--- fixes/fixconnections.py
+++ fixes/fixconnections.py
@@ -12,11 +12,25 @@ else:
 total = c.execute("SELECT count(*) FROM (SELECT id FROM tags WHERE name LIKE '%:%' OFFSET $1) AS beep",(skip,))[0][0]
 count = 0
 
+lastt = None
+def mayberetrans():
+	global lastt
+	import time
+	if lastt == None:
+		lastt = time.time()
+	else:
+		now = time.time()
+		if lastt - now < 20: return
+		lastt = now
+	db.retransaction()
+
 def doit():
 	global count
 	c.execute("CREATE TABLE IF NOT EXISTS toconnect (a int, b int, dis boolean DEFAULT FALSE NOT NULL)");
 	
-	for row in c.execute("select id,name from tags where name LIKE '%:%' ORDER BY id DESC OFFSET $1",(skip,)):
+	for row in c.execute("""select id,name from tags where name LIKE '%:%'
+  AND id NOT IN (select a FROM toconnect UNION SELECT b FROM toconnect)
+	ORDER BY id DESC OFFSET $1""",(skip,)):
 		wholetag,wholename = row
 		if wholename.startswith(':'): continue
 		count += 1
@@ -35,18 +49,23 @@ def doit():
 			c.execute("INSERT INTO toconnect (a,b,dis) VALUES ($1,$2,TRUE)",(wholetag,category))
 			c.execute("INSERT INTO toconnect (a,b) VALUES ($1,$2)",(tag,wholetag))
 			c.execute("INSERT INTO toconnect (a,b) VALUES ($1,$2)",(category,wholetag))
-		if count % 100 == 0:
-			if count > 1000:
+		if count % 2000 == 0:
+			if count > 10000:
 				c.execute("CREATE INDEX IF NOT EXISTS toconnect_a_dis  ON toconnect(a,dis)")
 				c.execute("CREATE INDEX IF NOT EXISTS toconnect_a  ON toconnect(a)")
 			print('boop')
-			for a, in c.execute("SELECT DISTINCT a FROM toconnect"):
-				print("reconnecting",a)
+			ctotal = c.execute("SELECT COUNT(*) FROM (SELECT DISTINCT a FROM toconnect) foo")[0][0]
+			i = 0
+			for a, in c.execute("SELECT DISTINCT a FROM toconnect ORDER BY a"):
+				print("reconnecting",a,i,ctotal)
+				i += 1
 				c.execute("""
 UPDATE things SET neighbors = array(SELECT unnest(neighbors) UNION SELECT b FROM toconnect WHERE a = $1 AND NOT dis EXCEPT SELECT b FROM toconnect WHERE a = $1 AND dis)
 WHERE neighbors && array(SELECT b FROM toconnect WHERE a = $1 AND NOT dis EXCEPT SELECT b FROM toconnect WHERE a = $1 AND dis)
 				""",(a,))
-			c.execute("
+				mayberetrans()
+			print("done reconnecting")
+			c.execute("DELETE FROM toconnect")
 			db.retransaction()
 			with open("derpcounter","w") as out:
 				out.write(str(skip+count))
