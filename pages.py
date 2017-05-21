e8af047d54bbff537f4a13577c7c9f03297921be
autocommit <autocommit>
2017-05-21 18:07:40 +0000

    auto 0 126 1901

--- pages.py
+++ pages.py
@@ -385,7 +385,7 @@ def makeLink(id,type,name,doScale,width=None,height=None,style=None):
 					d.param(name='src',value=thing)
 					o.commit()
 					note(o.contents)
-				wrapper = (o,dd.br,"Download")
+				wrapper = o
 		return fid,wrapper,thing
 	if type == 'application/x-shockwave-flash':
 		with d.NoParent:
