cfa1f4381bf40cb7d0e01499e5b5c57ec87a32af
autocommit <autocommit>
2017-05-25 19:46:15 +0000

    auto 0 141 2090

--- withtags.py
+++ withtags.py
@@ -125,9 +125,9 @@ def tagStatement(tags,offset=0,limit=0x30,taglimit=0x10,wantRelated=False):
 	clauses = {}
 
 	if tags.nega:
-		nega = Type(arg([getTag(tag) if isinstance(tag,str) else tag for tag in tags.nega]),'int[]')
+		nega = Type(arg([getTag(tag) if isinstance(tag,str) else tag for tag in tags.nega]),'int[]',True)
 		notWanted = EQ('things.id',ANY(nega))
-		print("yey",nega.clause)
+		print("yey",nega)
 		raise SystemExit
 		if tags.posi:
 			notWanted = AND(notWanted,
