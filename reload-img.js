7ad5ddd285a62b5e278e468b0bbf03f20cb5b758
autocommit <autocommit>
2017-05-25 06:00:47 +0000

    auto 0 131 1966

--- reload-img.js
+++ reload-img.js
@@ -2,14 +2,14 @@ function reload_later(dest,src) {
 	console.log("um",src);
 	var img = document.createElement('img');
 	function setsrc() {
-		img.style.width = '182px';
-		img.style.height = '182px';
 		img.src = src;
 	}
 	img.addEventListener('error', function(e) {
 		setTimeout(setsrc,1000);
 	},false);
 	img.addEventListener('load',function() {
+		img.style.width = '182px';
+		img.style.height = '182px';
 		dest.parentNode.replaceChild(img,dest);
 	},false);
 	setTimeout(setsrc,1000);
