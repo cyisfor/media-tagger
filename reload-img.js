c99b03d6bc70de50e82ef664a2e3342c95dc9240
autocommit <autocommit>
2017-05-25 06:10:16 +0000

    auto 0 125 1881

--- reload-img.js
+++ reload-img.js
@@ -3,6 +3,7 @@ function reload_later(dest,src) {
 	var img = document.createElement('img');
 	function setsrc() {
 		img.style.width = '219px';
+		img.style.height = '219px';
 		img.src = src;
 	}
 	img.addEventListener('error', function(e) {
