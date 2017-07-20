f1dd10e8fbba73b279e62cece1e69a4105ff789c
autocommit <autocommit>
2017-07-20 19:23:27 +0000

    auto 0 63 611

--- sql/implications.sql
+++ sql/implications.sql
@@ -13,15 +13,16 @@ BEGIN
     INSERT INTO impresult (tag) VALUES (_tag);
 	RAISE NOTICE 'found tag % % % %s',_returned,_depth,_tag,(select name from tags where id = _tag);
     _count := _count + 1;
-	SELECT COUNT(*) INTO _count FROM
-	  (SELECT * FROM implications(
-		             other.id,
+	FOR _neighbor IN SELECT other.id FROM tags
+	  inner join things on things.id = tags.id ,
+		tags other
+		WHERE tags.id = _tag AND other.id = ANY(things.neighbors) AND tags.complexity < other.complexity
+		LOOP
+		_count := _count + COUNT(*) FROM implications(
+		             _neighbor,
 								 _returned + _count,
-								 1 + _depth)
-        FROM tags inner join things on things.id = tags.id , tags other WHERE
-			 tags.id = _tag
-			 AND other.id = ANY(things.neighbors)
-             AND tags.complexity < other.complexity) AS imp;
+								 1 + _depth);
+	END LOOP;
 	RAISE NOTICE 'implications %',_count;
 	RETURN _count;
 END
