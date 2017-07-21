820aef01f414e68b2f70fd4124864f5d65124f66
autocommit <autocommit>
2017-07-21 02:53:27 +0000

    auto 0 13 170

--- test/pollsleep_eventlet.py
+++ test/pollsleep_eventlet.py
@@ -1,7 +1,11 @@
 import eventlet
-from eventlet.green.http.server import HTTPServer,BaseHTTPRequestHandler
-from socketserver import _ServerSelector
+from eventlet.green import selectors
+print(dir(selectors))
 import selectors
+print(dir(selectors))
+from eventlet.green.http.server import HTTPServer,BaseHTTPRequestHandler
+import socketserver
+socketserver._ServerSelector = selectors.SelectSelector
 
 def later():
 	print("ummm")
