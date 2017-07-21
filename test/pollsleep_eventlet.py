86cddc2e7ec66cf6a41c0ab7fab5daf425899972
autocommit <autocommit>
2017-07-21 01:57:36 +0000

    auto 0 22 338

--- test/pollsleep_eventlet.py
+++ test/pollsleep_eventlet.py
@@ -1,10 +1,13 @@
 import eventlet
-from eventlet.green.http.server import HTTPServer,BaseHTTPRequestHandler
+from eventlet.green import socket, selectors
+import selectors as boop
+print(boop,selectors)
 
 def connectandhang():
     c = socket.socket()
     ip = socket.gethostbyname("localhost")
     c.connect((ip, 14234))
+		
     return c.recv(1024)
 
 def later():
@@ -14,8 +17,4 @@ def later():
 # make sure something hanging listening on 14234
 
 eventlet.spawn(later)
-class Handle(BaseHTTPRequestHandler):
-	def do_GET(self):
-		print("get got")
-		self.send_response(997,"boop")
-HTTPServer(("127.0.0.1",14234),Handle).serve_forever()
+connectandhang()
