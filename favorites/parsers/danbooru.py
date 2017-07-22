a1ce1476278743468fada3afab4f845d2395a2f9
autocommit <autocommit>
2017-07-22 03:57:18 +0000

    auto 0 4 85

--- favorites/parsers/danbooru.py
+++ favorites/parsers/danbooru.py
@@ -19,7 +19,7 @@ def extract(doc):
 		class dims: pass
 		for n,v in notestyle.findall(note['style']):
 			setattr(dims,n,int(v))
-		body = note.find(attrs={'class':'note-body'})
+		body = note.next_element.contents[0]
 		print(body)
 		raise SystemExit
 
