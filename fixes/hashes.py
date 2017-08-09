af3d94894430fbbe4d6c5a84de7dbe53278f4fcc
autocommit <autocommit>
2017-08-09 10:40:06 +0000

    auto 0 29 330

--- fixes/hashes.py
+++ fixes/hashes.py
@@ -2,7 +2,7 @@ import create
 import db
 
 import hashlib
-
+import shutil
 import filedb
 import sys
 ident = int(sys.argv[1],16)
@@ -10,7 +10,7 @@ ident = int(sys.argv[1],16)
 with open(filedb.mediaPath(ident),"rb") as inp:
 	digest = create.mediaHash(inp)
 	inp.seek(0,0)
-	md5 = hashlib.MD5()
+	md5 = hashlib.md5()
 	shutil.copyfileobj(inp,create.writer(md5.update))
 	md5 = md5.hexdigest()
 
