0407206b1ce7b0de0d0a1dda27aaa63bbd2e4463
autocommit <autocommit>
2017-08-27 04:22:05 +0000

    auto 0 31 396

--- create.py
+++ create.py
@@ -271,11 +271,9 @@ def update(id,sources,tags,name):
 		db.execute("UPDATE media SET name = coalesce($3,name), sources = array(SELECT unnest(sources) from media where id = $2 UNION SELECT unnest($1::INTEGER[])), modified = clock_timestamp() WHERE id = $2",([source.id for source in sources],id,name))
 		oldTags = db.execute("SELECT neighbors FROM things WHERE id = $1",(id,))[0][0]
 		tags = tagsModule.toids_nega(tags)
-		oldTags.sort()
-		tags.sort()
-		print(set(tags)-set(oldTags))
-		raise SystemExit
-		tagsModule.tag(id,tags)
+		tags = set(tags)-set(oldTags)
+		if tags:
+			tagsModule.tag(id,tags)
 
 def internet(download,media,tags,primarySource,otherSources,name=None):
 	if not name:
