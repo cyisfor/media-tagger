bb1dbbd2a6185d94d0fcdf91ef9180255c810649
autocommit <autocommit>
2017-08-27 04:14:16 +0000

    auto 0 27 346

--- favorites/direct.py
+++ favorites/direct.py
@@ -7,17 +7,18 @@ from favorites.parse import parse,ParseError
 from favorites import parsers
 
 if __name__ == '__main__':
-    import select
-    import sys,io
-    if len(sys.argv)>1:
-        parse(sys.argv[1])
-        print("Nothing went wrong yay")
-    else:
-        sys.stdin = io.TextIOWrapper(sys.stdin.detach(),'utf-8')
-        for line in sys.stdin:
-            print('parsing',line.encode('utf-8'))
-            try: parse(line.strip())
-            except ParseError: pass
-            except Exception as e:
-                print(e)
-                time.sleep(3)
+	import select
+	import sys,io
+	if len(sys.argv)>1:
+		parse(sys.argv[1])
+		print("Nothing went wrong yay")
+	else:
+		sys.stdin = io.TextIOWrapper(sys.stdin.detach(),'utf-8')
+		for line in sys.stdin:
+			print('parsing',line.encode('utf-8'))
+			try: parse(line.strip())
+			except ParseError: pass
+			except Exception as e:
+				import traceback
+				traceback.print_exc()
+				time.sleep(3)
