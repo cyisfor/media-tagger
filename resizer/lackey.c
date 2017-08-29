84b3c0ac140a987998b8b6e0872869134893c15d
autocommit <autocommit>
2017-08-29 09:12:32 +0000

    auto 0 115 1604

--- resizer/lackey.c
+++ resizer/lackey.c
@@ -15,13 +15,13 @@ extern char* environ[];
 
 #define WORKER_IDLE 3600
 
-void run_worker(int efd) {
+int main(int argc, char** argv) {
   //struct rlimit memlimit = { 0x20000000, 0x20000000 };
   //setrlimit(RLIMIT_DATA,&memlimit);
   //setrlimit(RLIMIT_STACK,&memlimit);
   //setrlimit(RLIMIT_AS,&memlimit);
-  setproctitle_init(0,NULL,environ);
-  //recordInit();
+  setproctitle_init(argc,argv,environ);
+  recordInit();
   filedb_top(".");
 	record(INFO,"Started a lackey!");
   make_init();
@@ -30,7 +30,7 @@ void run_worker(int efd) {
 	alarm(WORKER_IDLE);
 
 	for(;;) {
-		ensure_eq(sizeof(m),read(efd,&m,sizeof(m)));
+		ensure_eq(sizeof(m),read(STDIN_FILENO,&m,sizeof(m)));
 		puts("boop");
 		if(m.width > 0)
 			make_resized(ctx,m.id,m.width);
@@ -47,4 +47,5 @@ void run_worker(int efd) {
 
 		alarm(WORKER_IDLE);
 	}
-}
+}		
+
