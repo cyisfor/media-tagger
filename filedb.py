6f6f500af3ed16922869450c12d60e03ea92a771
autocommit <autocommit>
2017-08-29 05:38:20 +0000

    auto 0 47 557

--- filedb.py
+++ filedb.py
@@ -26,64 +26,66 @@ def incoming(id,contents=None):
 	return _incoming('{:x}'.format(id),contents)
 	
 def _check(id,category,create=True,width=0):
-		medium = oj(base,category,id)
-		if os.path.exists(medium): return id,True
-		if not create:
-			return id, False
-		resizer.queue(id,width)
-		return id, os.path.exists(medium)
+	xid = '{:x}'.format(id)
+	medium = oj(base,category,xid)
+	if os.path.exists(medium): return xid,True
+	if not create:
+		return xid, False
+	resizer.queue(id,width)
+	return xid, os.path.exists(medium)
 
 def check(id,**kw):
-		kw.setdefault('category','thumb')
-		if kw.get('create') and kw.get('category') == 'thumb':
-				try:
-						del kw['width']
-				except KeyError: pass
-		return _check(id,**kw)
+	kw.setdefault('category','thumb')
+	if kw.get('create') and kw.get('category') == 'thumb':
+		try:
+				del kw['width']
+		except KeyError: pass
+	return _check(id,**kw)
 
 def checkResized(id,**kw):
-		return _check(id,'resized',800,**kw)
+	return _check(id,'resized',800,**kw)
 
 def checkOEmbed(id,maxWidth,**kw):
-		return _check(id,'oembed',maxWidth,**kw)
+	return _check(id,'oembed',maxWidth,**kw)
 
 def mediaPath(id=None):
-		loc = os.path.join(base,'media')
-		if id:
-				return os.path.join(loc,'{:x}'.format(id))
-		return loc
+	loc = os.path.join(base,'media')
+	if id:
+		return os.path.join(loc,'{:x}'.format(id))
+	return loc
 def thumbPath(id=None):
-		loc = os.path.join(base,'thumb')
-		if id:
-				return os.path.join(loc,'{:x}'.format(id))
-		return loc
+	loc = os.path.join(base,'thumb')
+	if id:
+		return os.path.join(loc,'{:x}'.format(id))
+	return loc
+
 def uploadPath(name):
-		return os.path.join(base,'uploads',name)
+	return os.path.join(base,'uploads',name)
 
 def MediaBecomer(dir=None):
-		if isinstance(dir,type('')):
-				self = tempfile.NamedTemporaryFile(dir=dir)
-		elif dir is None:
-				self = tempfile.NamedTemporaryFile(dir=os.path.join(base,'temp'))
-		else:
-				print(type(dir))
-				self = dir
-		def become(id):
-				medium = mediaPath(id)
-				if os.path.exists(medium): return
-				print('creating',medium)
-				os.chmod(self.name,0o644)
-				os.rename(self.name,medium)
-				try: self.close()
-				except OSError: pass
-		setattr(self,'become',become)
-		return self
+	if isinstance(dir,type('')):
+		self = tempfile.NamedTemporaryFile(dir=dir)
+	elif dir is None:
+		self = tempfile.NamedTemporaryFile(dir=os.path.join(base,'temp'))
+	else:
+		print(type(dir))
+		self = dir
+	def become(id):
+		medium = mediaPath(id)
+		if os.path.exists(medium): return
+		print('creating',medium)
+		os.chmod(self.name,0o644)
+		os.rename(self.name,medium)
+		try: self.close()
+		except OSError: pass
+	setattr(self,'become',become)
+	return self
 
 @contextmanager
 def mediaBecomer():
-		th = None
-		try:
-				th = MediaBecomer()
-				yield th
-		finally:
-				if th: th.close()
+	th = None
+	try:
+		th = MediaBecomer()
+		yield th
+	finally:
+		if th: th.close()
