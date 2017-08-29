38ed6b49c996e1a262bae6bb9629434a44d78b3c
autocommit <autocommit>
2017-08-29 04:26:20 +0000

    auto 0 29 327

--- resizer/__init__.py
+++ resizer/__init__.py
@@ -4,4 +4,9 @@ import sys,os
 here = os.path.dirname(sys.modules[__name__].__file__)
 if not here: here = os.curdir
 lib = os.path.join(here,"python.so");
+
+from ctypes import cdll
+
+lib = cdll.LoadLibrary(lib)
 print(lib)
+
