8e2bcd0c4dad342ce34c576b51ed8a9903ffb371
autocommit <autocommit>
2017-08-29 05:45:43 +0000

    auto 0 33 387

--- resizer/__init__.py
+++ resizer/__init__.py
@@ -7,13 +7,14 @@ lib = os.path.join(here,"python.so");
 import subprocess as s
 s.check_call(["make","-s","-C",here,"python.so"])
 
-from ctypes import cdll,c_int,c_uint
+from ctypes import cdll,c_int,c_uint,c_char_p
 
 os.chdir("/home/.local/filedb")
 
 def init(base):
 	global queue
 	l = cdll.LoadLibrary(lib)
+	l.init.argtypes = [c_char_p]
 	l.init(base)
 	queue = lambda id,width=0: l.queue(id,width or 0)
 	l.queue.argtypes = [c_uint, c_uint]
