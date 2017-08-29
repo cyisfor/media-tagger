3a630c5c236c79f01d34238af7424275fae9c862
autocommit <autocommit>
2017-08-29 04:59:46 +0000

    auto 0 29 412

--- resizer/__init__.py
+++ resizer/__init__.py
@@ -17,11 +17,12 @@ def init():
 	queue = lambda id,width=0: l.queue(dest,id,width)
 	l.queue.argtypes = [c_int, c_uint, c_uint]
 
+import subprocess as s
+s.check_call(["make","-C",here,"python.so"])
+	
 try:
 	init()
 except (AttributeError,OSError):
-	import subprocess as s
-	s.check_call(["make","-C",here,"python.so"])
 	init()
 
 queue(0x7f851)
