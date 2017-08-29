0366fd9fb2368f139c160c255c25f11c2c6e62e0
autocommit <autocommit>
2017-08-29 04:23:07 +0000

    auto 0 43 450

--- resizer/message.h
+++ resizer/message.h
@@ -1,12 +1,8 @@
+#include <stdint.h>
 #include <stdbool.h>
 
 struct message {
-	bool resize:1;
-	union {
-		struct {
-			uint32_t id:31;
-			uint32_t width:32;
-		} resized;
-		uint32_t id:31;
-	};
+	uint32_t id:32;
+	// 0 means don't resize, just make a default thumbnail
+	uint32_t width:32;
 } __attribute__((packed));
