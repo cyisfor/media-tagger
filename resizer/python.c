00c23ef1f8e343429167d377ebdf604dbca9ced4
autocommit <autocommit>
2017-08-29 04:22:18 +0000

    auto 0 74 704

---
+++ resizer/python.c
@@ -0,0 +1,18 @@
+#include <fcntl.h> // O_*, open
+#include <unistd.h> // write
+#include <assert.h>
+
+// a python ctypes stub for sending IDs as messages...
+
+int python_resize_init(void) {
+	return open("incoming/queue",O_RDONLY|O_NONBLOCK);
+}
+
+void python_queue_resize(int dest, uint32_t id, uint32_t width) {
+	struct message m = {
+		.id: id,
+		.width: width
+	};
+	ssize_t amt = write(dest,&m,sizeof(m));
+	assert(amt == sizeof(m));
+}
