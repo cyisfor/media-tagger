ae2e99cbc06f61b34bda24b930f29253f4a6727a
autocommit <autocommit>
2017-08-29 04:32:28 +0000

    auto 0 32 350

--- resizer/__init__.py
+++ resizer/__init__.py
@@ -5,15 +5,15 @@ here = os.path.dirname(sys.modules[__name__].__file__)
 if not here: here = os.curdir
 lib = os.path.join(here,"python.so");
 
-from ctypes import cdll
+from ctypes import cdll,c_int,c_uint
 
 def init():
 	global dest, queue
 	l = cdll.LoadLibrary(lib)
+	print(l.init)
 	dest = l.init()
 	queue = l.queue
 	queue.argtypes = [c_int, c_uint, c_uint]
-
 
 try:
 	init()
