33609bb5a5d1fbef90cc3139d355eecd51aff805
autocommit <autocommit>
2017-08-29 04:45:09 +0000

    auto 0 33 392

--- resizer/ensure.h
+++ resizer/ensure.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h> // abort
 
-#define ERROR(...) fprintf(stderr, ## __VA_ARGS__)
+#define ERROR(fmt, ...) fprintf(stderr, fmt "\n", ## __VA_ARGS__)
 
 
 #define ensure0(test) { typeof(test) res = (test);	\
