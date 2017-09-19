d2969d6a21dd0850ba0f143db0f3c6f4743a53eb
autocommit <autocommit>
2017-09-19 01:35:21 +0000

    auto 0 63 717

--- fixes/undeleted-media.py
+++ fixes/undeleted-media.py
@@ -2,10 +2,18 @@ import db
 import filedb
 import os
 
-paths = set(int(s,16) for s in os.listdir(filedb.mediaPath()))
-media = set(db.execute("SELECT id FROM media WHERE id = ANY($1::int[])",(paths,)))
-
 print("cd",filedb.mediaPath())
+
+def gewdint(s):
+	try: return int(s)
+	except ValueError:
+		print("rm",s)
+		return None
+
+paths = (gewdint(s,16) for s in os.listdir(filedb.mediaPath()))
+paths = (path for path in paths if path is not None)
+paths = set(paths)
+media = set(db.execute("SELECT id FROM media WHERE id = ANY($1::int[])",(paths,)))
 
 for path in paths - media:
 	print("rm {:x}".format(path))
