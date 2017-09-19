d9a375d4d7f225a8833540582c53dfd8b37c84d6
autocommit <autocommit>
2017-09-19 01:07:09 +0000

    auto 0 25 333

--- create.py
+++ create.py
@@ -271,9 +271,10 @@ def update(id,sources,tags,name):
 		db.execute("UPDATE media SET name = coalesce($3,name), sources = array(SELECT unnest(sources) from media where id = $2 UNION SELECT unnest($1::INTEGER[])), modified = clock_timestamp() WHERE id = $2",([source.id for source in sources],id,name))
 		oldTags = db.execute("SELECT neighbors FROM things WHERE id = $1",(id,))[0][0]
 		tags = tagsModule.toids_nega(tags)
+		note("boop")
 		if oldTags:
 			# only new tags please
-			print(tags,oldTags)
+			note.yellow(tags,"old",oldTags)
 			tags = set(tags)-set(oldTags)
 		if tags:
 			for tag in tags:
