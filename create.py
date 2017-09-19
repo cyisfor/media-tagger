93a808d395906b2390494f19349ad12dda1066bd
autocommit <autocommit>
2017-09-19 01:09:17 +0000

    auto 0 34 460

--- create.py
+++ create.py
@@ -267,12 +267,10 @@ tagsModule = tags
 
 def update(id,sources,tags,name):
 	donetags = []
-	note.blue("uhhh",tags)
 	with db.transaction():
 		db.execute("UPDATE media SET name = coalesce($3,name), sources = array(SELECT unnest(sources) from media where id = $2 UNION SELECT unnest($1::INTEGER[])), modified = clock_timestamp() WHERE id = $2",([source.id for source in sources],id,name))
 		oldTags = db.execute("SELECT neighbors FROM things WHERE id = $1",(id,))[0][0]
-		tags = tagsModule.toids_nega(tags)
-		note("boop")
+		tags = tagsModule.toids_posi(tags)
 		if oldTags:
 			# only new tags please
 			note.yellow(tags,"old",oldTags)
@@ -280,7 +278,7 @@ def update(id,sources,tags,name):
 		if tags:
 			for tag in tags:
 				assert tag is not None, tags
-			note.red("setting tags",tags)
+			raise RuntimeError("setting tags",tags)
 			tagsModule.tag(id,tags)
 
 def internet(download,media,tags,primarySource,otherSources,name=None):
