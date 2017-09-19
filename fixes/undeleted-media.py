5551b6637d63a94dcf4932ca5487ee21999e9ed2
autocommit <autocommit>
2017-09-19 01:33:00 +0000

    auto 0 46 535

---
+++ fixes/undeleted-media.py
@@ -0,0 +1,11 @@
+import db
+import filedb
+import os
+
+paths = set(int(s) for s in os.listdir(filedb.mediaPath()))
+media = set(db.execute("SELECT id FROM media WHERE id = ANY($1::int[])",(paths,)))
+
+print("cd",filedb.mediaPath())
+
+for path in paths - media:
+	print("rm {:x}".format(path))
