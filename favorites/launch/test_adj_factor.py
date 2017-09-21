0d55c8118029a7f3096098db4b90c1d798d7af59
autocommit <autocommit>
2017-09-21 20:30:11 +0000

    auto 0 352 4116

---
+++ favorites/launch/test_adj_factor.py
@@ -0,0 +1,8 @@
+def one(progress,total):
+	factor = total // 0xFFFF
+	progress/factor, (total // factor - 1), factor
+
+print(one(1000000,1000000))
+print(one(0,1000000))
+print(one(1000000-1,1000000))
+print(one(1000000+1,1000000))
