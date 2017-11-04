dc97f76a7dfb13b7a1de7ab9ec29c6531d1baf02
autocommit <autocommit>
2017-11-04 07:11:02 +0000

    auto 0 4 44

--- dedup/intersect.sh
+++ dedup/intersect.sh
@@ -2,6 +2,6 @@ while sleep 1; do
     { 
         echo '\pset format unaligned'
         echo '\pset tuples_only on'
-        echo 'select findDupes(0.4)'
+        echo "select count(1) FROM findDupes(0.4, '30 seconds'::interval)"
     } | psql pics | tail -n1 | grep -q '^0$' && break; 
 done
